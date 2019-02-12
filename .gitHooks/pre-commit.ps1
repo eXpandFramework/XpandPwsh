9f19966b28c6884c0be88a5f11fd20dcf4e54fc0
apobekiaris <apostolis.bekiaris@gmail.com>
2019-02-12 04:37:27 +0200

    added badges in ReadMe

--- .gitHooks/pre-commit.ps1
+++ .gitHooks/pre-commit.ps1
@@ -4,6 +4,7 @@ if (!(Get-Module PSMD -ListAvailable)){
 Import-Module PSMD
 Remove-Item "$PSScriptRoot\..\ReadMe.md" -Force
 New-PSMDDocument -Name ReadMe -OutputPath "$PSScriptRoot\.." -Content {
+    Paragraph -Text "![](https://img.shields.io/powershellgallery/v/XpandPosh.svg?style=flat) ![](https://img.shields.io/powershellgallery/dt/XpandPosh.svg?style=flat)"
     Title -Text Installation -Size h1
     Paragraph -Text "``XpandPosh`` is available in ``PSGallery``. Open a powershell prompt and type:"
     CodeBlock -Lang ps1 -Code "Install-Module XpandPosh"
