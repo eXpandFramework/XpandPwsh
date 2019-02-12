522bd5553896a201e75778aec3006e49e7c0f751
apobekiaris <apostolis.bekiaris@gmail.com>
2019-02-12 08:42:29 +0200

    Do not publish the same version

--- .gitHooks/UpdateVersion.ps1
+++ .gitHooks/UpdateVersion.ps1
@@ -1,14 +1,18 @@
-$onlineVersion=New-Object System.Version ((Find-Module XpandPosh -Repository PSGallery).Version)
-$file="$PSScriptRoot\..\XpandPosh\XpandPosh.psd1"
-$data=Get-Content $file -Raw
-$manifest=Invoke-Expression $data
-$newBuild =$onlineVersion.Build +1
-$newVersion="$($onlineVersion.Major).$($onlineVersion.Minor).$($newBuild)"
-if ($manifest.ModuleVersion -ne $newVersion){
-    Set-Content $file $data.Replace($manifest.ModuleVersion,$newVersion)
-    & git commit -a -m $newVersion
-    & git tag $newVersion
-    Write-Error "Version Changed" -ErrorAction Continue
-    exit 1
+$lastSha = (git ls-remote https://github.com/eXpandFramework/XpandPosh.git|Where {$_ -like "*HEAD*"}|Select -first 1).Replace("HEAD", "").Trim("")
+$needNewVersion = git diff --name-only $lastSha HEAD|Where-Object {$_ -like "*\XpandPosh\*" -and $_ -notlike "*.md"}
+if ($needNewVersion) {
+    $onlineVersion = New-Object System.Version ((Find-Module XpandPosh -Repository PSGallery).Version)
+    $file = "$PSScriptRoot\..\XpandPosh\XpandPosh.psd1"
+    $data = Get-Content $file -Raw
+    $manifest = Invoke-Expression $data
+    $newBuild = $onlineVersion.Build + 1
+    $newVersion = "$($onlineVersion.Major).$($onlineVersion.Minor).$($newBuild)"
+    if ($manifest.ModuleVersion -ne $newVersion) {
+        Set-Content $file $data.Replace($manifest.ModuleVersion, $newVersion)
+        & git commit -a -m $newVersion
+        & git tag $newVersion
+        Write-Error "Version Changed" -ErrorAction Continue
+        exit 1
+    }
 }
 
