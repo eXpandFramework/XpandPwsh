8b5c9d5837807b63e04f4da9696279ea37458e73
apobekiaris <apostolis.bekiaris@gmail.com>
2019-03-16 15:24:30 +0200

    remove Unsued files

--- .gitHooks/UpdateVersion.ps1
+++ .gitHooks/UpdateVersion.ps1
@@ -1,29 +1,29 @@
-function NeedsMinor {
-    param(
-        $moduleVersion
-    )
-    $c = New-Object System.Net.WebClient
-    $readme = $c.DownloadString("https://raw.githubusercontent.com/eXpandFramework/XpandPosh/master/ReadMe.md")
-    $onlineModules = $readme.Split("`r`n")|ForEach-Object {
-        if ($start -and ($_ -eq "``````")) {
-            $start = $false
-        }
-        if ($start) {
-            $_
-        }
-        if ($_ -eq "``````txt") {
-            $start = $true
-        }
-    }|Where-Object {$_}
-    $localModules=(Get-Command -Module XpandPosh)|Select-Object -ExpandProperty Name
-    $newCommands=$localModules|Where-Object{!$onlineModules.Contains("$_")}
-    $removedCommand=$onlineModules|Where-Object{!$localModules.Contains("$_")}
-    if ($($newCommands+$removedCommand).Count -gt 0){
-        $onlineVersion = New-Object System.Version ((Find-Module XpandPosh -Repository PSGallery).Version)
-        return $onlineVersion -gt $moduleVersion
-    }
-    $false
-}
+# function NeedsMinor {
+#     param(
+#         $moduleVersion
+#     )
+#     $c = New-Object System.Net.WebClient
+#     $readme = $c.DownloadString("https://raw.githubusercontent.com/eXpandFramework/XpandPosh/master/ReadMe.md")
+#     $onlineModules = $readme.Split("`r`n")|ForEach-Object {
+#         if ($start -and ($_ -eq "``````")) {
+#             $start = $false
+#         }
+#         if ($start) {
+#             $_
+#         }
+#         if ($_ -eq "``````txt") {
+#             $start = $true
+#         }
+#     }|Where-Object {$_}
+#     $localModules=(Get-Command -Module XpandPosh)|Select-Object -ExpandProperty Name
+#     $newCommands=$localModules|Where-Object{!$onlineModules.Contains("$_")}
+#     $removedCommand=$onlineModules|Where-Object{!$localModules.Contains("$_")}
+#     if ($($newCommands+$removedCommand).Count -gt 0){
+#         $onlineVersion = New-Object System.Version ((Find-Module XpandPosh -Repository PSGallery).Version)
+#         return $onlineVersion -gt $moduleVersion
+#     }
+#     $false
+# }
 if (!(Get-Module XpandPosh -ListAvailable)) {
     Install-Module XpandPosh
 }
@@ -31,15 +31,14 @@ $lastSha = Get-GitLastSha "https://github.com/eXpandFramework/XpandPosh.git"
 $lastSha
 $needNewVersion = (git diff --name-only "$lastSha" HEAD|Select-Object -First 1)|Where-Object {$_ -like "XpandPosh/*" -and $_ -notlike "*.md" -and $_ -notlike "*.yml" }
 "needNewVersion=$needNewVersion"
-if ($needNewVersion) {
+$needNewMinor = git diff --name-only |Where-Object{$_ -like "*ReadMe.md"}
+"needNewMinor=$needNewMinor"
+if ($needNewVersion -or $needNewMinor) {
     $file = "$PSScriptRoot\..\XpandPosh\XpandPosh.psd1"
     $data = Get-Content $file -Raw
     $manifest = Invoke-Expression $data
     $moduleVersion = New-Object System.Version($manifest.ModuleVersion)
     "moduleVersion=$moduleVersion"
-    $needNewMinor = NeedsMinor $moduleVersion
-    "needNewMinor=$needNewMinor"
-    
     if ($needNewMinor) {
         $newMinor = $moduleVersion.Minor + 1
         $newBuild = 0
