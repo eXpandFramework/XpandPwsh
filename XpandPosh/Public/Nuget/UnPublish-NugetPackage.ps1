838ba2b44c0017d59e1c1ba2f40832f9e5352641
apobekiaris <apostolis.bekiaris@gmail.com>
2019-03-19 14:46:45 +0200

    UnPublish-NugetPackage

--- XpandPosh/Public/Nuget/UnPublish-NugetPackage.ps1
+++ XpandPosh/Public/Nuget/UnPublish-NugetPackage.ps1
@@ -1,13 +1,22 @@
 function UnPublish-NugetPackage {
     [CmdletBinding()]
     param (
-        $Source
+        [parameter(Mandatory,ValueFromPipeline)]
+        [string]$Name,
+        [parameter(Mandatory)]
+        [string]$NugetApiKey,
+        [parameter(Mandatory)]
+        [string]$Source,
+        [parameter(Mandatory)]
+        [string]$Version
     )
     
     begin {
+        $nuget=Get-NugetPath
     }
     
     process {
+        & $nuget Delete $name $version -Source $source -ApiKey $NugetApiKey -NonInteractive
     }
     
     end {
