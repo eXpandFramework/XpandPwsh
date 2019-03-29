58c79ecd3d0411fa7f93253c7ee1eda8f4335493
apobekiaris <apostolis.bekiaris@gmail.com>
2019-03-29 09:18:13 +0200

    Update-ProjectTargetFramework use xml VS hangs

--- XpandPosh/Public/VS/Update-ProjectTargetFramework.ps1
+++ XpandPosh/Public/VS/Update-ProjectTargetFramework.ps1
@@ -10,12 +10,24 @@ function Update-ProjectTargetFramework {
     
     process {
         $allProjects=Get-Project -All
+        $allProjects
         $activity="Changing TargetFramework to $FrameworkVersion"
         $activity
-        $allProjects|Invoke-Parallel -ActivityName $activity -VariablesToImport "FrameworkVersion" -Script {
-            "Changing $($_.Name)"
-            ($_.Properties|Where-Object{$_.Name -match "TargetFrameworkMoniker"}).Value=".NETFramework,Version=v$FrameworkVersion"
+        $dte=($allProjects|Select-Object -First 1 ).Dte
+        $solutionFile=$dte.Solution.Filename
+        $allProjects|ForEach-Object{
+            $projectName=$_.ProjectName
+            [xml]$csproj=Get-Content $_.FullName
+            $csproj.Project.PropertyGroup|Where-Object{$_["TargetFrameworkVersion"]}|ForEach-Object{
+                "Changing $projectName $($_.TargetFrameworkVersion) to $FrameWorkVersion"
+                $_.TargetFrameworkVersion="v$FrameworkVersion"
+            }
+            $csproj.Save($_.FullName)
         }
+        "Closing solution $($dte.Solution.FileName)"
+        $dte.Solution.Close($true)
+        "Opening solution $solutionFile"
+        $dte.Solution.Open($solutionFile)
     }
     
     end {
