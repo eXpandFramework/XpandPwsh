0a3ba5aadfcb8541967641730c5dfa184ab7a0c8
apobekiaris <apostolis.bekiaris@gmail.com>
2019-04-30 17:51:32 +0300

    Publish-NugetPackage update due to other functions refactors

--- XpandPosh/Public/Nuget/Publish-NugetPackage.ps1
+++ XpandPosh/Public/Nuget/Publish-NugetPackage.ps1
@@ -21,29 +21,26 @@ function Publish-NugetPackage {
         Write-Verbose "Packages found:"
         $packages|Write-Verbose
         
-        $published=$packages|Select-Object -ExpandProperty Id| Invoke-Parallel -activityName "Getting latest versions from sources" -VariablesToImport @("Source") -Script  { 
-            Get-NugetPackageSearchMetadata -Name $_ -Sources $Source
-        } 
+        $published=$packages|Select-Object -ExpandProperty Id| Get-NugetPackageSearchMetadata  -Source $Source 
         Write-Verbose "Published packages:"
-        $published=$published|Select-object -ExpandProperty Metadata|Get-NugetPackageMetadataVersion
+        $published=$published|Get-NugetPackageMetadataVersion
         $published|Write-Verbose 
         
         $needPush=$packages|Where-Object{
             $p=$_
             $published |Where-Object{
-                $_.Name -eq $p.Name -and $_.Version -eq $_.Version
+                $_.Name -eq $p.Id -and $_.Version -eq $_.Version
             }
         }
         Write-Verbose "NeedPush"
         $needPush|Write-Verbose 
         $NupkgPath=$NupkgPath.TrimEnd("\")
-        $publishScript={        
-            $package="$NupkgPath\$($_.Name).$($_.Version).nupkg"
-            "Pushing $package in $Source "
+        
+        $needPush|Invoke-Parallel -ActivityName "Publishing Nugets" -VariablesToImport @("ApiKey","NupkgPath","Source","Nuget") -Script {
+            $package="$NupkgPath\$($_.Id).$($_.Version).nupkg"
+            Write-Output "Pushing $package in $Source "
             & $Nuget Push "$package" -ApiKey $ApiKey -source $Source
         }
-        
-        $needPush|Invoke-Parallel -ActivityName "Publishing Nugets" -VariablesToImport @("ApiKey","NupkgPath","Source","Nuget") -Script $publishScript
      
     }
     end {
