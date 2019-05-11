4c382701484f05d3d38f99a1db7c78d0ae63a035
apobekiaris <apostolis.bekiaris@gmail.com>
2019-05-11 07:24:17 +0300

    Invoke-AzureRestMethod

---
+++ XpandPosh/Public/AzureDevOps/Invoke-AzureRestMethod.ps1
@@ -0,0 +1,27 @@
+function Invoke-AzureRestMethod {
+    [CmdletBinding()]
+    param (
+        [parameter(Mandatory)]
+        [string]$Token,
+        [parameter(Mandatory)]
+        [string]$Organization,
+        [parameter(Mandatory)]
+        [string]$Project,
+        [parameter(Mandatory)]
+        [string]$Resource,
+        [string]$Version = "5.0"
+    )
+    
+    begin {
+    }
+    
+    process {
+        $encodedPat = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes(":$Token"))
+        $uri="https://dev.azure.com/$Organization/$Project/_apis/$Resource`?api-version=$Version"
+        $resp = Invoke-RestMethod -Uri $uri -Headers @{Authorization = "Basic $encodedPat" }
+        $resp.value|ForEach-Object{$_}
+    }
+    
+    end {
+    }
+}
--- XpandPosh/XpandPosh.psd1
+++ XpandPosh/XpandPosh.psd1
@@ -70,6 +70,7 @@
 
     # Functions to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no functions to export.
     FunctionsToExport = @(
+        "Invoke-AzureRestMethod",
         "Install-DebugOptimizationHook",
         "Publish-AssemblyToGac",
         "Compress-Project",
