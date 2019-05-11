f3949a657db79317e358f1d0142c22e0dad1f4cd
apobekiaris <apostolis.bekiaris@gmail.com>
2019-05-11 07:24:04 +0300

    Find-XpandPackage faills for all param

--- XpandPosh/Public/Nuget/Find-XpandPackage.ps1
+++ XpandPosh/Public/Nuget/Find-XpandPackage.ps1
@@ -21,7 +21,7 @@ function Find-XpandPackage {
             $p = & ($nuget) list -Source (Get-PackageFeed -Xpand)| ConvertTo-PackageObject | Where-Object { $_.Id -like $Filter }
         }
         elseif ($PackageSource -eq "All") {
-            $p = (Find-XpandPackage -Name $Filter -PackageSource Lab) + (Find-XpandPackage -Name $Filter -PackageSource Release)
+            $p = $(Find-XpandPackage -Filter $Filter -PackageSource Lab) , $(Find-XpandPackage -Filter $Filter -PackageSource Release)
         }
         elseif ($PackageSource -eq "Release") {
             $p = & $nuget list author:eXpandFramework -Source (Get-PackageFeed -Nuget)|ConvertTo-PackageObject | Where-Object { $_.Id -like $Filter }
