29c739d7d6e36207c995e537e28d72ad4eb46dd5
apobekiaris <apostolis.bekiaris@gmail.com>
2019-06-05 08:36:17 +0300

    Clear-ProjectDirectories: path parameter not respected #1

--- XpandPosh/Public/System/Clear-ProjectDirectories.ps1
+++ XpandPosh/Public/System/Clear-ProjectDirectories.ps1
@@ -7,9 +7,9 @@ function Clear-ProjectDirectories {
         Where-Object { $_.Name -eq 'bin' -or $_.Name -eq 'obj' -or $_.Name -eq '.vs' -or $_.Name.StartsWith('_ReSharper')} | ForEach-Object{
             (Get-Item $_.FullName).Delete($true)
         }
-    Get-ChildItem -Include "*.log", "*.bak" -Recurse | Remove-Item -Force
+    Get-ChildItem $path -Include "*.log", "*.bak" -Recurse | Remove-Item -Force
     $folders = "packages"
-    Get-ChildItem -Recurse | 
+    Get-ChildItem $path -Recurse | 
         Where-Object { $_.PSIsContainer} | 
         Where-Object { $folders -contains $_.Name   } | 
         Remove-Item -Force -Recurse
