5885eda301e7f791dfd2c39055986ecca227a869
apobekiaris <apostolis.bekiaris@gmail.com>
2019-06-10 00:35:19 +0300

    post-commit

--- .gitHooks/FunctionsToExport.ps1
+++ .gitHooks/FunctionsToExport.ps1
@@ -1,9 +1,14 @@
 $GitHubUser="eXpandFramework"
+Set-Location $PSScriptRoot
 $scriptFunctions=Get-childitem "..\XpandPwsh\Public" *.ps1 -Recurse|ForEach-Object{
     $_.BaseName
 }
+Write-Host "ScriptFunctions:" -f Blue
+$scriptFunctions
 Import-Module "..\XpandPwsh\Cmdlets\bin\XpandPwsh.Cmdlets.dll"
 $exportedCommands=(Get-Module XpandPwsh.Cmdlets).ExportedCommands.Keys
+Write-Host "ExportedCommands:" -f Blue
+$exportedCommands
 $functions=(($scriptFunctions+$exportedCommands)|Sort-Object|ForEach-Object{
     "`"$_`""
 }) -join ",`r`n"
@@ -19,8 +24,9 @@ FunctionsToExport = @(
 
 Set-Content "..\XpandPwsh\XpandPwsh.psd1" $psd1.Trim()
 $lastSha = Get-GitLastSha "https://github.com/$GitHubUser/XpandPwsh.git"
+Write-Host "lastSha=$lastSha" -f Blue
 if (git diff --name-only "$lastSha" HEAD|Where-Object{$_ -like "*.psd1"}){
-    Write-Error "Manifest changed"
+    New-BurntToastNotification -Text "Manifest changed" -Sound 'Alarm2' -SnoozeAndDismiss
     exit 1
 }
 
