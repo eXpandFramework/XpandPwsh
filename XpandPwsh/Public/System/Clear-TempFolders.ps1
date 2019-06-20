c6f0a07d665cd53a8d076e4af3f949e459f2bad9
apobekiaris <apostolis.bekiaris@gmail.com>
2019-06-20 11:03:54 +0300

    Clear-TempFolders

---
+++ XpandPwsh/Public/System/Clear-TempFolders.ps1
@@ -0,0 +1,6 @@
+function Clear-TempFolders {
+    param(
+        $tempfolders = @( "C:\Windows\Temp\*", "C:\Windows\Prefetch\*", "C:\Documents and Settings\*\Local Settings\temp\*", "C:\Users\*\Appdata\Local\Temp\*" )
+    )
+    Remove-Item $tempfolders -force -recurse
+}
