09b91c548a376d8d3afaebb3284ba2a4fca40afc
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2019-06-24 01:17:16 +0300

    Invoke-Parallel: ConcurrencyLimit support

--- XpandPwsh/Cmdlets/src/XpandPwsh.Cmdlets/InvokeParallel/Invoke-Parallel.cs
+++ XpandPwsh/Cmdlets/src/XpandPwsh.Cmdlets/InvokeParallel/Invoke-Parallel.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Concurrent;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Management.Automation;
 using System.Management.Automation.Runspaces;
@@ -14,8 +15,8 @@ namespace XpandPwsh.Cmdlets.InvokeParallel{
     [Cmdlet(VerbsLifecycle.Invoke, "Parallel")]
     [CmdletBinding]
     public class InvokeParallel : XpandCmdlet{
-        private ConcurrentBag<object> _values;
         private PSVariable[] _psVariables;
+        private ConcurrentBag<object> _values;
 
         [Parameter]
         public override string ActivityName{ get; set; }
@@ -28,6 +29,7 @@ namespace XpandPwsh.Cmdlets.InvokeParallel{
 
         [Parameter(Mandatory = true, Position = 0, ValueFromPipeline = true)]
         public object Value{ get; set; }
+
         [Parameter]
         public override int ActivityId{ get; set; }
 
@@ -35,13 +37,19 @@ namespace XpandPwsh.Cmdlets.InvokeParallel{
         public int RetryOnError{ get; set; } = 0;
 
         [Parameter]
-        public int RetryDelay{ get; set; } = 3000;
+        public int LimitConcurrency{ get; set; } 
+
         [Parameter]
-        public int StepInterval{ get; set; } 
+        public int RetryDelay{ get; set; } = 3000;
+
+        [Parameter]
+        public int StepInterval{ get; set; }
+
         protected override Task BeginProcessingAsync(){
             _values = new ConcurrentBag<object>();
             _psVariables = this.Invoke<PSVariable>("Get-Variable")
                 .Where(variable => VariablesToImport.Contains(variable.Name)).ToArray();
+
             return base.BeginProcessingAsync();
         }
 
@@ -51,41 +59,49 @@ namespace XpandPwsh.Cmdlets.InvokeParallel{
         }
 
         protected override Task EndProcessingAsync(){
-            if (!_values.Any()){
-                return Task.CompletedTask;
-            }
-            var signal = Enumerable.Range(0,RetryOnError).ToObservable()
-                .Delay(TimeSpan.FromMilliseconds(RetryDelay)).Publish().AutoConnect();
-            var eventLoopScheduler = new EventLoopScheduler(start => new Thread(start));
-            var synchronizationContext = SynchronizationContext.Current;
+            if (!_values.Any()) return Task.CompletedTask;
+            
             var values = _values.ToObservable();
             if (StepInterval > 0){
-                values=values.StepInterval(TimeSpan.FromMilliseconds(StepInterval),eventLoopScheduler);
+                var eventLoopScheduler = new EventLoopScheduler(start => new Thread(start));
+                values = values.StepInterval(TimeSpan.FromMilliseconds(StepInterval), eventLoopScheduler);
             }
+
+            var retrySignal = Enumerable.Range(0, RetryOnError).ToObservable()
+                .Delay(TimeSpan.FromMilliseconds(RetryDelay)).Publish().AutoConnect();
+            values = LimitConcurrency > 0 ? InvokeWithLimit(values, retrySignal) : values.SelectMany(o => Start(o, retrySignal));
+
             return values
-                .SelectMany((o, i) => Observable.Start(() => {
-                        using (var runspace = RunspaceFactory.CreateRunspace()){
-                            runspace.Open();
-                            runspace.SetVariable(new PSVariable("_", o));
-                            runspace.SetVariable(_psVariables);
-                            var psObjects = runspace.Invoke(Script.ToString());
-                            var lastExitCode = runspace.Invoke("$LastExitCode").FirstOrDefault();
-                            if (lastExitCode != null&& ((int) lastExitCode.BaseObject) >0){
-                                var error = string.Join(Environment.NewLine,runspace.Invoke("$Error"));
-                                if (!string.IsNullOrWhiteSpace(error)){
-                                    throw new Exception($"ExitCode:{lastExitCode}{Environment.NewLine}Errors: {error}{Environment.NewLine}Script:{Script}");
-                                }
-                            }
-                            runspace.Close();
-                            return psObjects;
-                        }
-                    })
-                    .RetryWhen(_ => _.SelectMany(exception => signal.Concat(Observable.Throw<int>(exception))))
-                    .HandleErrors(this, ActivityName,synchronizationContext))
-                .WriteObject(this,_values.Count)
+                .WriteObject(this, _values.Count)
+                .HandleErrors(this, ActivityName,SynchronizationContext.Current)
                 .ToTask();
         }
 
-        
+        private IObservable<Collection<PSObject>> InvokeWithLimit(IObservable<object> values,IObservable<int> retrySignal){
+            return values.Select((o, i) => Observable.Defer(() => Start(o,retrySignal)))
+                .Merge(LimitConcurrency);
+        }
+
+        private IObservable<Collection<PSObject>> Start(object o, IObservable<int> retrySignal){
+
+            return Observable.Start(() => {
+                using (var runspace = RunspaceFactory.CreateRunspace()){
+                    runspace.Open();
+                    runspace.SetVariable(new PSVariable("_", o));
+                    runspace.SetVariable(_psVariables);
+                    var psObjects = runspace.Invoke(Script.ToString());
+                    var lastExitCode = runspace.Invoke("$LastExitCode").FirstOrDefault();
+                    if (lastExitCode != null && (int) lastExitCode.BaseObject > 0){
+                        var error = string.Join(Environment.NewLine, runspace.Invoke("$Error"));
+                        if (!string.IsNullOrWhiteSpace(error))
+                            throw new Exception(
+                                $"ExitCode:{lastExitCode}{Environment.NewLine}Errors: {error}{Environment.NewLine}Script:{Script}");
+                    }
+
+                    runspace.Close();
+                    return psObjects;
+                }
+            }).RetryWhen(_ => _.SelectMany(exception => retrySignal.Concat(Observable.Throw<int>(exception))));
+        }
     }
 }
