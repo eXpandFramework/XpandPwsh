09b20b6eae410f87a130051beca6c87ce2d79048
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2019-06-24 01:18:02 +0300

    Use-MonoCecil: Check if loaded

--- XpandPwsh/Public/DotNet/Use-MonoCecil.ps1
+++ XpandPwsh/Public/DotNet/Use-MonoCecil.ps1
@@ -13,7 +13,10 @@ function Use-MonoCecil {
         if ($All) {
             $assemblies = 3
         }
-        $mono = Use-NugetAssembly Mono.Cecil *v4.0 $OutputFolder | Select-Object -First $assemblies
+        $mono = [System.AppDomain]::CurrentDomain.GetAssemblies() | Where-Object { $_.FullName -match "Mono.Cecil" } | Select-Object -First $assemblies
+        if (!$mono) {
+            $mono = Use-NugetAssembly Mono.Cecil *v4.0 $OutputFolder | Select-Object -First $assemblies
+        }
         $mono
         
     }
