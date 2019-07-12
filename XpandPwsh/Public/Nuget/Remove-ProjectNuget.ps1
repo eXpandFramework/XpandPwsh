17940bd515b855454f25653f216cba538cc2c336
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2019-07-13 02:05:23 +0300

    Remove-ProjectNuget:packagereference format support

--- XpandPwsh/Public/Nuget/Remove-ProjectNuget.ps1
+++ XpandPwsh/Public/Nuget/Remove-ProjectNuget.ps1
@@ -8,17 +8,34 @@ function Remove-ProjectNuget{
         [string]$ProjectFilter="*"
     )
     Get-ChildItem $projectPath "$ProjectFilter.csproj" -Recurse | ForEach-Object { 
-        [xml]$project=Get-XmlContent $_.FullName
-        $project.Project.ItemGroup.Reference|Where-Object{$_.Include -match "$id"}|ForEach-Object{
-            $fileName=[System.IO.Path]::GetFileName($_.Hintpath)
-            $hintPath="$nugetAssembliesBin\$fileName"
-            if (!(Test-Path $hintPath)){
-                throw "HintPath not found: $hintPath"
+        $path=$_.FullName
+        [xml]$project=Get-XmlContent $path
+        
+        if ($project.Project.ItemGroup.packageReference){
+            $refNode=($project.Project.ItemGroup.Reference|Select-Object -First 1).ParentNode
+            $project.Project.ItemGroup.packageReference|Where-Object{$_.Include -match "$id"}|ForEach-Object{
+                $_.ParentNode.RemoveChild($_)
+                $r=$project.CreateElement("Reference")    
+                $r.SetAttribute("Include",$_.Include)
+                $h=$project.CreateElement("HintPath")    
+                $h.InnerText="$nugetAssembliesBin\$($_.Include).dll"
+                $r.AppendChild($h)
+                $refNode.AppendChild($r)
+                $project.Save($path)
             }
-            $_.Hintpath= $hintPath
         }
-
-        $project.Save($_.FullName)
+        else{
+            $project.Project.ItemGroup.Reference|Where-Object{$_.Include -match "$id"}|ForEach-Object{
+                $fileName=[System.IO.Path]::GetFileName($_.Hintpath)
+                $hintPath="$nugetAssembliesBin\$fileName"
+                if (!(Test-Path $hintPath)){
+                    throw "HintPath not found: $hintPath"
+                }
+                $_.Hintpath= $hintPath
+            }
+            $project.Save($path)
+        }
+        
     } 
     push-location $projectPath
     Clear-ProjectDirectories 
