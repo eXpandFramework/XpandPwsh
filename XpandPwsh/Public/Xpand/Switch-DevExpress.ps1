577c525ccab43981247e29018370cedaf8d7aa2c
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2019-07-13 02:05:03 +0300

    Switch-DevExpress

---
+++ XpandPwsh/Public/Xpand/Switch-DevExpress.ps1
@@ -0,0 +1,56 @@
+function Switch-DevExpress {
+    [alias("Switch-DX")]
+    param(
+        [parameter(Mandatory)]
+        [string[]]$dxNugetPackagesPath ,
+        [parameter(Mandatory)]
+        [string]$sourcePath 
+        
+    )
+    New-Item "$dxNugetPackagesPath\Nupkg" -ItemType Directory -ErrorAction SilentlyContinue
+    Get-ChildItem $dxNugetPackagesPath *.nupkg|ForEach-Object{
+        if (!(Test-Path "$dxNugetPackagesPath\Nupkg\$($_.BaseName).zip")){
+            Move-Item $_.FullName "$dxNugetPackagesPath\Nupkg\$($_.BaseName).zip"
+            Expand-Archive "$dxNugetPackagesPath\Nupkg\$($_.BaseName).zip" "$dxNugetPackagesPath\Nupkg\$($_.BaseName)"
+            Remove-Item "$dxNugetPackagesPath\Nupkg\$($_.BaseName).zip"
+        }
+    }
+    
+    Get-ChildItem $sourcePath *.csproj -Recurse | ForEach-Object {
+        $projectPath=$_.FullName
+        [xml]$project = Get-XmlContent $projectPath
+        $dxReferences = $project.project.ItemGroup.Reference | Where-Object{$_.Include -like "DevExpress*"}
+        if ($dxReferences) {
+            $packageReferences = $project.project.ItemGroup.PackageReference
+            if (!$packageReferences) {
+                $group = $project.DocumentElement.CreateElement("ItemGroup")
+                $project.project.AppendChild($group)
+            }
+            else{
+                $group=$packageReferences.ParentNode
+            }
+            $dxReferences |ForEach-Object {
+                $_.ParentNode.RemoveChild($_)
+                $name=$_.Include
+                $package=get-childItem "$dxNugetPackagesPath\Nupkg\" "$name.dll" -Recurse|Select-Object -First 1
+                
+                if (!$package){
+                    throw "$name not found in $($projectPath)"
+                }
+                $packageName=(Get-Item "$($package.DirectoryName)\..\..\").BaseName
+                $regex=[regex] '(?ix)\.[\d]*\.[\d]*\.[\d]*'
+                $Version=$regex.Match($packageName).Value.Trim(".")
+                $packageName=$regex.Replace($packageName,"")
+                $packageReference = $project.CreateElement("PackageReference")
+                $packageReference.SetAttribute("Include",$packageName)
+                $packageReference.SetAttribute("Version",$Version)
+                $group.AppendChild($project.CreateTextNode([System.Environment]::NewLine))|Out-Null
+                $group.AppendChild($project.CreateTextNode("    "))|Out-Null
+                $group.AppendChild($packageReference)|Out-Null
+                $group.AppendChild($project.CreateTextNode([System.Environment]::NewLine))|Out-Null
+                $project.Save($projectPath)
+                $projectPath|Remove-BlankLines
+            }
+        }
+    }
+}
