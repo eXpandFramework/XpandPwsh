c718e20e3f497c1f2da88017084a351cfdf7f323
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2019-08-21 02:35:21 +0300

    Start-VS: Display msg if not elevated

--- XpandPwsh/Private/Start-VS.ps1
+++ XpandPwsh/Private/Start-VS.ps1
@@ -1,4 +1,11 @@
-$env:COMPLUS_ZapDisable=1
+$isElevated = [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")
+if (!$isElevated) {
+    Add-Type -AssemblyName PresentationFramework
+    [System.Windows.MessageBox]::Show('Run as Admin')
+}
+    
+$env:COMPLUS_ZapDisable = 1
+    
 Push-Location 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\'
 move-item devenv.exe _devenv.exe
 & $args[0] $args[1]
