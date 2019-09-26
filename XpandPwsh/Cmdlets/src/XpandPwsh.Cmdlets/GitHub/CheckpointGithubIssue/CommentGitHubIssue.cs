fec44fc3cc486ba7add61200364a159c3d9d7f12
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2019-09-26 21:22:06 +0300

    CommentGitHubIssue

---
+++ XpandPwsh/Cmdlets/src/XpandPwsh.Cmdlets/GitHub/CheckpointGithubIssue/CommentGitHubIssue.cs
@@ -0,0 +1,24 @@
+﻿using System.Management.Automation;
+using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
+using System.Threading.Tasks;
+
+namespace XpandPwsh.Cmdlets.GitHub.CheckpointGithubIssue{
+    [CmdletBinding(SupportsShouldProcess = true)]
+    [Cmdlet(VerbsCommon.New, "GitHubComment", SupportsShouldProcess = true)]
+    public class CommentGitHubIssue : GitHubCmdlet{
+        [Parameter]
+        public int IssueNumber{ get; set; }
+        [Parameter(Mandatory = true)]
+        public string Comment{ get; set; }
+        [Parameter(Mandatory = true)]
+        public string Repository{ get; set; }
+
+        protected override Task ProcessRecordAsync(){
+            GitHubClient.Repository.Get(Owner, Repository)
+                .ToObservable().SelectMany(repository =>
+                    GitHubClient.Issue.Comment.Create(repository.Id, IssueNumber, Comment));
+            return base.ProcessRecordAsync();
+        }
+    }
+}
