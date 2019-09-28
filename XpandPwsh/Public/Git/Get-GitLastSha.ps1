203c520de0408e02aa65474dcdee88fa3a408bb0
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2019-09-29 00:46:42 +0300

    Get-GitLastSha: Add Branch Support

--- XpandPwsh/Public/Git/Get-GitLastSha.ps1
+++ XpandPwsh/Public/Git/Get-GitLastSha.ps1
@@ -2,14 +2,29 @@ function Get-GitLastSha {
     [CmdletBinding()]
     param (
         [parameter(ValueFromPipeline,Mandatory)]
-        [string]$repoGitUrl
+        [string]$repoGitUrl,
+        [string]$Branch
     )
     
     begin {
     }
     
     process {
-        (git ls-remote $repoGitUrl|Where-Object {$_ -like "*HEAD*"}|Select-Object -first 1).Replace("HEAD", "").Trim("")
+        git ls-remote $repoGitUrl|ForEach-Object{
+            $regex = [regex] '(\w*) *(.*)'
+            $sha = $regex.Match($_).Groups[1].Value
+            $ref=$regex.Match($_).Groups[2].Value.Trim()
+            if (!$Branch){
+                if ($ref -eq "HEAD"){
+                    $sha
+                }
+            }
+            else{
+                if ($ref.Replace("refs/heads/","") -eq $Branch){
+                    $sha
+                }
+            }
+        }|Select-Object -First 1
     }
     
     end {
