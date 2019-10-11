3ca908576eb40d476601427fa0bdbc741b4beec4
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2019-10-11 09:06:41 +0300

    Update-NugetPackage: improve filtering

--- XpandPwsh/Public/Nuget/Update-NugetPackage.ps1
+++ XpandPwsh/Public/Nuget/Update-NugetPackage.ps1
@@ -2,63 +2,85 @@
 function Update-NugetPackage {
     [cmdletbinding()]
     param(
-        [parameter(ValueFromPipeline,ParameterSetName="SourcePath")]
+        [parameter(ValueFromPipeline, ParameterSetName = "SourcePath")]
         [string]$SourcePath = ".",
-        [parameter(ParameterSetName="projects")]
+        [parameter(ParameterSetName = "projects")]
         [System.IO.FileInfo[]]$projects,
         [string]$RepositoryPath,
         [parameter()]
-        [string]$Filter = "*",
+        [string]$Filter = ".*",
+        [string]$ExcludeFilter ,
         [string]$sources = ((Get-PackageSourceLocations Nuget) -join ";")
     )
-    write-host "PackagesConfig" -f Blue 
-    Update-NugetPackagesConfig $SourcePath $RepositoryPath $Filter $sources
-    write-host "PackageReference" -f blu
-    if (!$projects){
-        $projects=Get-ChildItem $SourcePath *.csproj -Recurse 
-    }
-    write-host "projects:" -f blue
-    
-    $packages=$projects|ForEach-Object{
-        [xml]$csproj=Get-Content $_.FullName
-        $csproj.Project.ItemGroup.PackageReference.Include|Where-Object{$_ -and $_ -like $Filter}
-    }|Sort-object -Unique
-    write-host "packages:" -f blue
-    $packages
-    $metadata=$packages|ForEach-Object{
-        Get-NugetPackageSearchMetadata $_ $sources
+    Write-Host "PackagesConfig" -f Blue 
+    if ($RepositoryPath) {
+        Update-NugetPackagesConfig $SourcePath $RepositoryPath $Filter $ExcludeFilter $sources
     }
     
-    $packagesToAdd=$projects|ForEach-Object{
-        $csprojPath=$_.FullName
-        $csprojName=$_.BaseName
-        [xml]$csproj=Get-Content $csprojPath
-        $csproj.Project.ItemGroup.PackageReference|Where-Object{$_.Include -and $_.Include -like $Filter}|ForEach-Object{
-            $p=$_.Include
-            $m=$metadata|Where-Object{$_.Identity.Id -eq $p}
-            $latestVersion = (Get-NugetPackageMetadataVersion $m).version
-            $installedVersion=$_.Version
-            if ($latestVersion -ne $installedVersion){
-                Write-host "Updating $csprojName $p $installedVersion to $latestVersion" -f Green
-                [PSCustomObject]@{
-                    ProjectPath = $csprojPath
-                    Package=$p
-                    Version=$latestVersion
-                    Sources=$sources
-                }
+    Write-Host "PackageReference" -f blue
+    if (!$projects) {
+        $projects = Get-ChildItem $SourcePath *.csproj -Recurse 
+    }
+    Write-Host "projects:" -f blue
+    $projects | Select-Object -expandProperty baseName
+    $packages = $projects | ForEach-Object {
+        [xml]$csproj = Get-Content $_.FullName
+        $p = $csproj.Project.ItemGroup.PackageReference.Include | Where-Object { $_ -and $_ -match $Filter }
+        if ($ExcludFilter) {
+            $p | Where-Object { $_ -notmatch $ExcludeFilter }
+        }
+        else {
+            $p
+        }
+    } | Sort-Object -Unique
+Write-Host "packages:" -f blue
+$packages
+$metadata = $packages | ForEach-Object {
+    Get-NugetPackageSearchMetadata $_ $sources
+}
+    
+$packagesToAdd = $projects | ForEach-Object {
+    $csprojPath = $_.FullName
+    $csprojName = $_.BaseName
+    [xml]$csproj = Get-Content $csprojPath
+    $csproj.Project.ItemGroup.PackageReference | Where-Object {
+        $r=$_.Include -and $_.Include -match $Filter
+        if ($ExcludeFilter -and $r){
+            $_.Include -notmatch $ExcludeFilter
+        }
+        else {
+            $r
+        }
+    } | ForEach-Object {
+        $p = $_.Include
+        $m = $metadata | Where-Object { $_.Identity.Id -eq $p }
+        $latestVersion = (Get-NugetPackageMetadataVersion $m).version
+        $installedVersion = $_.Version
+        if ($latestVersion -ne $installedVersion) {
+            Write-Host "Updating $csprojName $p $installedVersion to $latestVersion" -f Green
+            [PSCustomObject]@{
+                ProjectPath = $csprojPath
+                Package     = $p
+                Version     = $latestVersion
+                Sources     = $sources
             }
         }
     }
-    $packagesToAdd|Write-Verbose
-    if ($packagesToAdd){
-        # $packagesToAdd|Invoke-Parallel -StepInterval 100 -Script{
-        $packagesToAdd|foreach{
-            dotnet add $_.ProjectPath package $_.Package -v $_.Version -s $_.Sources |Write-Verbose
-            IF ($LASTEXITCODE){
-                throw
-            }
+}
+$packagesToAdd | Write-Verbose
+if ($packagesToAdd) {
+    # $packagesToAdd|Invoke-Parallel -StepInterval 100 -Script{
+    $packagesToAdd | ForEach-Object {
+        $projectPath = $_.ProjectPath
+        $p = $_.Package
+        $s = $_.Sources
+        $v = $_.Version
+        dotnet add $projectPath package $p -v $v -s $s 
+        IF ($LASTEXITCODE) {
+            throw
         }
     }
+}
     
 }
 
@@ -69,7 +91,8 @@ function Update-NugetPackagesConfig {
         [string]$SourcePath = ".",
         [string]$RepositoryPath,
         [parameter()]
-        [string]$Filter = "*",
+        [string]$Filter = ".*",
+        [string]$ExcludFilter,
         [string]$sources = ((Get-PackageSourceLocations Nuget) -join ";")
     )
     
@@ -84,7 +107,7 @@ function Update-NugetPackagesConfig {
             }
         }
     
-        $metadatas = $configs.Content.packages.package.id | Where-Object { $_ -like $Filter } | Select-Object -Unique | Get-NugetPackageSearchMetadata -Source $sources
+        $metadatas = $configs.Content.packages.package.id | Where-Object { $_ -match $Filter -and $_ -notmatch $ExcludFilter } | Select-Object -Unique | Get-NugetPackageSearchMetadata -Source $sources
         $metadatas | ForEach-Object {
             [PSCustomObject]@{
                 Title   = $_.Title
@@ -96,9 +119,9 @@ function Update-NugetPackagesConfig {
             $config = $_.Config
             $_.Content.packages.package | Where-Object { $_.id -like $filter } | ForEach-Object {
                 $packageId = $_.Id
-                $metadata = $metadatas | Where-object { $_.Identity.Id -eq $packageId }
+                $metadata = $metadatas | Where-Object { $_.Identity.Id -eq $packageId }
                 if ($metadata) {
-                    $csproj = Get-ChildItem $config.DirectoryName *.csproj | Select -first 1
+                    $csproj = Get-ChildItem $config.DirectoryName *.csproj | Select-Object -first 1
                     $latestVersion = (Get-NugetPackageMetadataVersion $metadata).version
                     [PSCustomObject]@{
                         Id         = $packageId
@@ -110,29 +133,29 @@ function Update-NugetPackagesConfig {
                 }
             }
         } | Where-Object { $_.NewVersion -and ($_.Version -ne $_.NewVersion) }
-        $sortedPackages = $packages | Group-Object Config | ForEach-Object {
-            $p = [PSCustomObject]@{
-                Packages = ($_.Group | Get-SortedPackageByDependencies)
-            }
-            $p
-        } 
-    
-    
-        $sortedPackages | Invoke-Parallel -activityName "Update all packages" -VariablesToImport @("RepositoryPath", "sources") -Script {
-            ($_.Packages | ForEach-Object {
-                    if ($RepositoryPath) {
-                        & (Get-NugetPath) Update $_.Config -Id $_.Id -Version $($_.NewVersion) -Source $sources -NonInteractive -RepositoryPath $RepositoryPath
-                    }
-                    else {
-                        & (Get-NugetPath) Update $_.Config -Id $_.Id -Version $($_.NewVersion) -Source $sources -NonInteractive 
-                    }
-            
-                })
+    $sortedPackages = $packages | Group-Object Config | ForEach-Object {
+        $p = [PSCustomObject]@{
+            Packages = ($_.Group | Get-SortedPackageByDependencies)
         }
-    }
+        $p
+    } 
     
-    end {
+    
+    $sortedPackages | Invoke-Parallel -activityName "Update all packages" -VariablesToImport @("RepositoryPath", "sources") -Script {
+        ($_.Packages | ForEach-Object {
+                if ($RepositoryPath) {
+                    & (Get-NugetPath) Update $_.Config -Id $_.Id -Version $($_.NewVersion) -Source $sources -NonInteractive -RepositoryPath $RepositoryPath
+                }
+                else {
+                    & (Get-NugetPath) Update $_.Config -Id $_.Id -Version $($_.NewVersion) -Source $sources -NonInteractive 
+                }
+            
+            })
     }
+}
+    
+end {
+}
 }
 
 function Get-SortedPackageByDependencies {
@@ -155,14 +178,14 @@ function Get-SortedPackageByDependencies {
         while ($all.Count) {
             $all | ForEach-Object {
                 $obj = $_
-                $deps = $obj.Metadata.Metadata.DependencySets.Packages | select -ExpandProperty Id
+                $deps = $obj.Metadata.Metadata.DependencySets.Packages | Select-Object -ExpandProperty Id
                 $exist = $all | Select-Object -ExpandProperty Id | Where-Object { $deps -contains $_ }
                 if (!$exist) {
-                    $list.Add($obj) | out-null
+                    $list.Add($obj) | Out-Null
                 }
             }
             $list | ForEach-Object {
-                $all.Remove($_) | out-null
+                $all.Remove($_) | Out-Null
             }
         }
         $list | ForEach-Object { $_ }
