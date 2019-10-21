b814b78eb4d900396ce8b3088e8dca9e6649cfa4
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2019-10-21 20:33:55 +0300

    Clear-NugetCache: Xpand ArgumentSet

--- XpandPwsh/Public/Nuget/Clear-NugetCache.ps1
+++ XpandPwsh/Public/Nuget/Clear-NugetCache.ps1
@@ -1,14 +1,23 @@
 function Clear-NugetCache {
     [CmdletBinding()]
     param (
-        
+        [ValidateSet("XpandPackages")]
+        $Filter
     )
     
     begin {
     }
     
     process {
-        & (Get-NugetPath) locals all -clear
+        if ($Filter) {
+            $path = (Get-NugetInstallationFolder GlobalPackagesFolder)
+            $folders=Get-ChildItem $path 
+            $folders|Where-Object{$_.BaseName -like "Xpand*" -or $_.BaseName -like "eXpand*" -and $_.BaseName -notlike "*VersionConverter"}|Remove-Item -Recurse -Force 
+        }
+        else {
+            & (Get-NugetPath) locals all -clear
+        }
+        
     }
     
     end {
