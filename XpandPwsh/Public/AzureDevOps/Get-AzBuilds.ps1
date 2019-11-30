3f5b1c0de548ace58ef542153a1acf4fd7ca3b5b
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2019-11-30 12:58:09 +0200

    minor update

--- XpandPwsh/Public/AzureDevOps/Get-AzBuilds.ps1
+++ XpandPwsh/Public/AzureDevOps/Get-AzBuilds.ps1
@@ -29,10 +29,11 @@ function Get-AzBuilds {
     process {
         $DefinitionIds=(Get-AzDefinition @cred|Where-Object{$_.Name -in $Definition}).id
         (Invoke-AzureRestMethod "build/builds" @cred)|Where-Object{
-            !$Status -or $_.Status -in $Status -and
-            !$reason -or $_.reason -in $reason -and
-            !$Result -or $_.Result -in $Result -and
-            !$DefinitionIds -or $_.Definition.id -in $DefinitionIds
+            $statusCheck=!$Status -or ( $_.Status -and $_.Status -in $Status)
+            $reasonChck=!$reason -or ($_.reason -and $_.reason -in $reason)
+            $resultCheck=!$Result -or ($_.Result  -and $_.Result -in $Result)
+            $definitionCheck=!$DefinitionIds -or $_.Definition.id -in $DefinitionIds
+            $statusCheck -and $reasonChck -and $resultCheck -and $definitionCheck
         }
     }
     
