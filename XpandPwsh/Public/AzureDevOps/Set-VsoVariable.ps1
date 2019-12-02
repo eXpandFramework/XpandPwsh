d6e2ed5820f3802a767f27e26ef1702caed4a778
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2019-12-02 03:49:42 +0200

    minor update

--- XpandPwsh/Public/AzureDevOps/Set-VsoVariable.ps1
+++ XpandPwsh/Public/AzureDevOps/Set-VsoVariable.ps1
@@ -2,6 +2,10 @@ function Set-VsoVariable {
     [CmdletBinding()]
     param (
         [parameter(ValueFromPipeline,Mandatory,Position=0)]
+        [ArgumentCompleter({
+            param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
+            @("build.updatebuildnumber")|where-object{$_ -like "$wordToComplete*"}
+        })]
         [string]$Name,
         [parameter(Position=1)]
         $Value
