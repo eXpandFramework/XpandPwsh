5edb0218b4fcd78a158f621fedb2581da16b101a
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2019-12-05 19:25:43 +0200

    minor update

--- XpandPwsh/Public/AzureDevOps/Add-AzBuild.ps1
+++ XpandPwsh/Public/AzureDevOps/Add-AzBuild.ps1
@@ -12,6 +12,7 @@ function Add-AzBuild {
         [switch]$KeepForEver,
         [switch]$StopOthers,
         [switch]$StopIfRunning,
+        [string]$Branch,
         [string]$Organization = $env:AzOrganization,
         [string]$Project = $env:AzProject,
         [string]$Token = $env:AzDevopsToken
@@ -27,14 +28,16 @@ function Add-AzBuild {
     
     process {
         if ($StopIfRunning) {
-            $Definition|Get-AzBuilds -Status inProgress, notStarted, postponed @cred | Remove-AzBuild @cred
+            $Definition | Get-AzBuilds -Status inProgress, notStarted, postponed @cred | Remove-AzBuild @cred
         }
-        $builds = ($Definition|Get-AzDefinition).id | ForEach-Object {
+        $builds = ($Definition | Get-AzDefinition).id | ForEach-Object {
             $body = @{
-                definition  = @{id = $_ }
-                parameters  = $Parameters | ConvertTo-Json
-                keepForEver = $KeepForEver.IsPresent
-            }| Remove-DefaultValueKeys 
+                definition   = @{id = $_ }
+                parameters   = $Parameters | ConvertTo-Json
+                keepForEver  = $KeepForEver.IsPresent
+                tags         = ($Tag -join ",")
+                sourceBranch = $Branch
+            } | Remove-DefaultValueKeys 
             Invoke-AzureRestMethod "build/builds" -Method Post -Body ($body | ConvertTo-Json) @cred
         }
         
--- XpandPwsh/XpandPwsh.psd1
+++ XpandPwsh/XpandPwsh.psd1
@@ -12,7 +12,7 @@
 RootModule = 'XpandPwsh.psm1'
 
 # Version number of this module.
-ModuleVersion = '0.38.14'
+ModuleVersion = '0.38.15'
 
 # Supported PSEditions
 # CompatiblePSEditions = @()
