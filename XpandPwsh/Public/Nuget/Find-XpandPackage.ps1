eb9627f34801d5c4c5b3355fc00dc3ab65db281e
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2020-01-19 13:24:09 +0200

    Find-XpandPackage: autocomplete on filter

--- XpandPwsh/Public/Nuget/Find-XpandPackage.ps1
+++ XpandPwsh/Public/Nuget/Find-XpandPackage.ps1
@@ -2,6 +2,18 @@ function Find-XpandPackage {
     [CmdletBinding()]
     param (
         [parameter(Mandatory, ValueFromPipeline, Position = 0)]
+        [ArgumentCompleter({
+            [OutputType([System.Management.Automation.CompletionResult])]  # zero to many
+            param(
+                [string] $CommandName,
+                [string] $ParameterName,
+                [string] $WordToComplete,
+                [System.Management.Automation.Language.CommandAst] $CommandAst,
+                [System.Collections.IDictionary] $FakeBoundParameters
+            )
+            
+            (find-xpandpackage "*$WordToComplete*" ).id
+        })]
         [string]$Filter,
         [parameter(Position = 1)]
         [ValidateSet("All", "Release", "Lab")]
