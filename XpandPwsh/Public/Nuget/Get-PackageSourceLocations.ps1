d0880c2df935c0399ad919d32f103f8d2267644d
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2020-01-19 13:25:16 +0200

    Get-PackageSourceLocations: add Name, AllTypes parameters

--- XpandPwsh/Public/Nuget/Get-PackageSourceLocations.ps1
+++ XpandPwsh/Public/Nuget/Get-PackageSourceLocations.ps1
@@ -1,5 +1,37 @@
-function Get-PackageSourceLocations($providerName){
-    $(Get-PackageSource|Where-object{
-        !$providerName -bor ($_.ProviderName -eq $providerName) 
-    }|Select-Object -ExpandProperty Location -Unique|Where-Object{$_ -like "http*" -or (Test-Path $_)})
+function Get-PackageSourceLocations {
+    [CmdletBinding()]
+    param (
+        [string]$ProviderName,
+        [ArgumentCompleter({
+            [OutputType([System.Management.Automation.CompletionResult])]  # zero to many
+            param(
+                [string] $CommandName,
+                [string] $ParameterName,
+                [string] $WordToComplete,
+                [System.Management.Automation.Language.CommandAst] $CommandAst,
+                [System.Collections.IDictionary] $FakeBoundParameters
+            )
+            (Get-PackageSource |Where-Object{$_.Name -like "$wordtocomplete*"}).Name
+        })]
+        [string]$Name,
+        [Switch]$AllTypes
+    )
+    
+    begin {
+        
+    }
+    
+    process {
+        $(Get-PackageSource|Where-object{
+            (($providerName -and $_.ProviderName -eq $providerName) -or ($Name -and $_.Name -eq $Name))
+        }|Select-Object -ExpandProperty Location -Unique|Where-Object{
+            if (!$AllTypes){
+                $_ -like "http*" -or (Test-Path $_)
+            }
+        })       
+    }
+    
+    end {
+        
+    }
 }
