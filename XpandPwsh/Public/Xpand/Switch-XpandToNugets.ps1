cd045959e5bfb6527e44cac03e05e502d9cca634
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2020-01-30 16:03:11 +0200

    1.201.5.8

--- XpandPwsh/Public/Xpand/Switch-XpandToNugets.ps1
+++ XpandPwsh/Public/Xpand/Switch-XpandToNugets.ps1
@@ -12,27 +12,48 @@ function Switch-XpandToNugets {
     }
     
     process {
-        $packages = & (Get-NugetPath) list -source $PackageSource|ConvertTo-PackageObject
-        Get-ChildItem $Path *.*proj -Recurse -Exclude "*Tests*" | ForEach-Object {
+        $pObjects = (& (Get-NugetPath) list -source $PackageSource | ConvertTo-PackageObject | Where-Object { $_.id -like "*Xpand*" } )
+        $packages = $pObjects | Invoke-Parallel -VariablesToImport "PackageSource" -Script { 
+        # $packages = $pObjects | foreach { 
+            $downloadResult=(Get-NugetPackage  -Name $_.id -ResultType DownloadResults -Source $PackageSource )
+            [PSCustomObject]@{
+                DownloadResult = $downloadResult
+                Version        = ([xml](Get-Content $downloadResult.PackageReader.GetNuspecFile())).package.metadata.version
+            }
+        } | ForEach-Object {
+            $packageReader = $_.DownloadResult.PackageReader
+            $version = $_.Version
+            $packageReader.GetLibItems().Items | ForEach-Object {
+                [PSCustomObject]@{
+                    Id       = $packageReader.NuspecReader.GetIdentity().Id
+                    FileName = [System.IO.Path]::GetFileNameWithoutExtension($_)
+                    Version  = $version
+                }    
+            }
+        } | Sort-Object FileName -Unique
+        $packages
+        Get-ChildItem $Path *.*proj -Recurse | ForEach-Object {
             [xml]$csproj = Get-Content $_
-            $projectPath=$_.FullName
+            $projectPath = $_.FullName
             $csproj.Project.ItemGroup.Reference | Where-Object { $_.include -like "Xpand*" } | ForEach-Object {
-                $packageReference = $csproj.CreateElement("PackageReference")
-                $packageReference.SetAttribute("Include", $_.Include)
-                $package = $packages | Where-Object { $_.Id -eq $packageReference.include }
-                if (!$package){
+                $include = $_.include
+                if ($include.Contains(",")) {
+                    $include = $include.SubString(0, $include.IndexOf(","))
+                }
+                $package = $packages | Where-Object { $_.FileName -eq $include }
+                if (!$package) {
                     throw "$($_.include) not found"
                 }
-                $packageReference.SetAttribute("Version", $package.Version)
-                $group=$_.ParentNode
-                $group.AppendChild($csproj.CreateTextNode([System.Environment]::NewLine)) | Out-Null
-                $group.AppendChild($csproj.CreateTextNode("    ")) | Out-Null
-                $group.AppendChild($packageReference) | Out-Null
-                $group.AppendChild($csproj.CreateTextNode([System.Environment]::NewLine)) | Out-Null
+                $group = $_.ParentNode
+                Add-XmlElement $csproj "PackageReference" "ItemGroup" ([ordered]@{
+                    Include=$package.Id
+                    Version = $package.Version
+                })
                 $group.RemoveChild($_)
             }
             $projectPath | Remove-BlankLines
             $csproj.Save($_)
+            Clear-ProjectDirectories $projectPath
         }
     }
     
--- XpandPwsh/XpandPwsh.psd1
+++ XpandPwsh/XpandPwsh.psd1
@@ -12,7 +12,7 @@
 RootModule = 'XpandPwsh.psm1'
 
 # Version number of this module.
-ModuleVersion = '1.201.5.6'
+ModuleVersion = '1.201.5.8'
 
 # Supported PSEditions
 # CompatiblePSEditions = @()
