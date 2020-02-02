82ff96d28c63eef88fb52ec7dc25ec3c6ec3d224
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2020-02-02 08:17:45 +0200

    Check-ReferenceConflict: add Filter, Recurse parameters

--- XpandPwsh/Cmdlets/src/XpandPwsh.Cmdlets/InvokeParallel/Check-ReferenceConflict.cs
+++ XpandPwsh/Cmdlets/src/XpandPwsh.Cmdlets/InvokeParallel/Check-ReferenceConflict.cs
@@ -3,40 +3,40 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Management.Automation;
-using System.Reactive.Linq;
-using System.Reactive.Threading.Tasks;
 using System.Reflection;
-using System.Threading.Tasks;
-using XpandPwsh.CmdLets;
 
 namespace XpandPwsh.Cmdlets.InvokeParallel{
     [Cmdlet(VerbsCommon.Get, "ReferenceConflict")]
     [CmdletBinding]
     public class GetReferenceConflict : PSCmdlet{
         [Parameter]
-        public string Path{ get; set; }
+        public string Path{ get; set; } 
 
+        [Parameter]
+        public string Filter{ get; set; } = "*";
+        [Parameter]
+        public  SwitchParameter Recurse{ get; set; }
         protected override void ProcessRecord(){
             base.ProcessRecord();
             if (Path == null) Path = Environment.CurrentDirectory;
             
             var multipleVersions = GetReferencedAssembliesWithMultipleVersions(Path).ToArray();
-//            var maxNameLength = multipleVersions.SelectMany(t => t).Max(t => t.assembly.Name.Length);
-            foreach (var x1 in multipleVersions.Select(tuples => new {
-                tuples.Key,Conflicts = tuples.Select(tuple => new{Assembly = tuple.assembly, Reference = tuple.referenceASsembly})
-            })) WriteObject(x1);
-//            return;
-//            foreach (var group in multipleVersions)
-//            {
-//                WriteObject($"Possible conflicts for {group.Key}:" );
-//                foreach (var reference in group)
-//                {
-//                    WriteObject($"    {reference.assembly.Name.PadRight(maxNameLength)} references {reference.referenceASsembly.FullName}");
-//                }
-//            }
+            var pairs = multipleVersions.Select(tuples => new{
+                tuples.Key, Conflicts = tuples.Select(tuple => new{Assembly = tuple.assembly, Reference = tuple.referenceASsembly})
+            });
+            var wildcardPattern = new WildcardPattern(Filter,WildcardOptions.Compiled|WildcardOptions.IgnoreCase);
+            var results = pairs.Where(__ =>wildcardPattern.IsMatch(__.Key)).ToArray();
+            if (results.Length == 1){
+                WriteObject(results.First().Conflicts);
+            }
+            else{
+                foreach (var pair in results){
+                    WriteObject(pair);
+                }
+            }
         }
 
-        public static IEnumerable<IGrouping<string, (AssemblyName assembly, AssemblyName referenceASsembly)>> GetReferencedAssembliesWithMultipleVersions(string path){
+        public IEnumerable<IGrouping<string, (AssemblyName assembly, AssemblyName referenceASsembly)>> GetReferencedAssembliesWithMultipleVersions(string path){
             var assemblies = GetAllAssemblies(path);
             var references = GetReferencesFromAllAssemblies(assemblies);
 
@@ -45,26 +45,25 @@ namespace XpandPwsh.Cmdlets.InvokeParallel{
                 .Where(r => r.Select(t => t.referenceASsembly.FullName).Distinct().Count() > 1);
         }
 
-        private static IEnumerable<(AssemblyName assembly, AssemblyName referenceASsembly)>
-            GetReferencesFromAllAssemblies(IEnumerable<Assembly> assemblies){
+        private IEnumerable<(AssemblyName assembly, AssemblyName referenceASsembly)> GetReferencesFromAllAssemblies(IEnumerable<Assembly> assemblies){
             return assemblies.SelectMany(GetReferencedAssemblies);
         }
 
-        private static IEnumerable<(AssemblyName assembly, AssemblyName referenceASsembly)> GetReferencedAssemblies(Assembly asm){
+        private  IEnumerable<(AssemblyName assembly, AssemblyName referenceASsembly)> GetReferencedAssemblies(Assembly asm){
             return asm.GetReferencedAssemblies().Select(refAsm => (asm.GetName(), refAsm));
         }
 
-        private static IEnumerable<Assembly> GetAllAssemblies(string path){
+        private  IEnumerable<Assembly> GetAllAssemblies(string path){
             return GetFileNames(path, "*.dll", "*.exe")
                 .Select(TryLoadAssembly)
                 .Where(asm => asm != null);
         }
 
-        private static IEnumerable<string> GetFileNames(string path, params string[] extensions){
-            return extensions.SelectMany(ext => Directory.GetFiles(path, ext, SearchOption.AllDirectories));
+        private  IEnumerable<string> GetFileNames(string path, params string[] extensions){
+            return extensions.SelectMany(ext => Directory.GetFiles(path, ext, Recurse ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly));
         }
 
-        private static Assembly TryLoadAssembly(string filename){
+        private  Assembly TryLoadAssembly(string filename){
             try{
                 return Assembly.LoadFile(filename);
             }
@@ -72,32 +71,5 @@ namespace XpandPwsh.Cmdlets.InvokeParallel{
                 return null;
             }
         }
-//        public static void Main(string[] args){
-//            foreach (var path in GetPathsFromArgs(args)){
-//                var references = AssemblyReference.GetReferencedAssembliesWithMultipleVersions(path);
-//                OutputConflicts(Console.Out, references);
-//            }
-//        }
-
-//        private static List<string> GetPathsFromArgs(string[] args){
-//            var paths = new List<string>();
-//
-//            foreach (var arg in args)
-//                if (arg.StartsWith("-")){
-//                    if (arg.StartsWith("--")){
-//                        // Long option.
-//                    }
-//                }
-//                else if (arg.StartsWith("/")){
-//                    // Windows option.
-//                }
-//                else{
-//                    paths.Add(arg);
-//                }
-//
-//            if (paths.Count == 0) paths.Add(Directory.GetCurrentDirectory());
-//
-//            return paths;
-//        }
     }
 }
