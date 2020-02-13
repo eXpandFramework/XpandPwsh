926c4383d7250fd8e88b3a73b5264d1d40fbb219
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2020-02-13 18:26:22 +0200

    find-Find-NugetPackage: add completer

--- XpandPwsh/Public/Nuget/Find-NugetPackage.ps1
+++ XpandPwsh/Public/Nuget/Find-NugetPackage.ps1
@@ -1,6 +1,18 @@
 function Find-NugetPackage {
     [CmdletBinding()]
     param (
+        [ArgumentCompleter({
+            [OutputType([System.Management.Automation.CompletionResult])]  # zero to many
+            param(
+                [string] $CommandName,
+                [string] $ParameterName,
+                [string] $WordToComplete,
+                [System.Management.Automation.Language.CommandAst] $CommandAst,
+                [System.Collections.IDictionary] $FakeBoundParameters
+            )
+            
+            (Find-NugetPackage -Name $WordToComplete).Id
+        })]
         [parameter(ValueFromPipeline,Mandatory)]
         [string]$Name,
         [switch]$AllVersions,
