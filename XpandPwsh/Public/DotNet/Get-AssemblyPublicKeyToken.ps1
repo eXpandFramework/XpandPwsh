48bc7dc5fd608454e9c92f6253da784ee64f5f89
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2020-02-13 18:26:00 +0200

    Get-AssemblyPublicKeyToken

---
+++ XpandPwsh/Public/DotNet/Get-AssemblyPublicKeyToken.ps1
@@ -0,0 +1,21 @@
+function Get-AssemblyPublicKeyToken {
+    [CmdletBinding()]
+    param (
+        [parameter(ValueFromPipeline,Mandatory)]
+        [System.IO.FileInfo]$Assembly
+    )
+    
+    begin {
+    }
+    
+    process {
+        
+        $asm=Mount-Assembly $Assembly.FullName
+        (($asm.GetName().GetPublicKeyToken()|ForEach-Object{
+            $_.ToString("x2")
+        }) -join "").Trim("")
+    }
+    
+    end {
+    }
+}
