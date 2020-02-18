1b949b0cde1b45456e5c7b2ac8962d3305dc0b3b
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2020-02-18 14:16:41 +0200

    Get-Get-AssemblyReference: recursive implmentation

--- XpandPwsh/Public/DotNet/Get-AssemblyReference.ps1
+++ XpandPwsh/Public/DotNet/Get-AssemblyReference.ps1
@@ -1,30 +1,54 @@
-function Get-AssemblyReference{
+function Get-AssemblyReference {
     [CmdletBinding()]
     param (
-        [parameter(ValueFromPipeline,Mandatory)]
+        [parameter(ValueFromPipeline, Mandatory)]
         [string]$AssemblyPath,
         [string]$NameFilter,
-        [string]$VersionFilter
+        [string]$VersionFilter,
+        [System.IO.FileInfo[]]$AssemblyList = (Get-ChildItem $AssemblyPath *.dll),
+        [Switch]$Recurse
     )
     
     begin {
-        Use-MonoCecil|Out-Null
+        Use-MonoCecil | Out-Null
     }
     
     process {
-        [Mono.Cecil.AssemblyDefinition]$assembly=[Mono.Cecil.AssemblyDefinition]::ReadAssembly($AssemblyPath)
-        $assembly.MainModule.AssemblyReferences|ForEach-Object{
+        [Mono.Cecil.AssemblyDefinition]$assembly = Read-AssemblyDefinition $AssemblyPath $AssemblyList
+        $refs = $assembly.MainModule.AssemblyReferences | ForEach-Object {
             [PSCustomObject]@{
-                Name = $_.Name
-                Version = $_.Version.ToString()
+                Name     = $_.Name
+                Version  = $_.Version.ToString()
                 FullName = $_.FullName
             }
-        }|Where-Object{
-            $nameMatch=!$NameFilter -or $_.Name -like $NameFilter
-            $versionMatch=!$VersionFilter -or $_.Version -like $VersionFilter 
+        } | Where-Object {
+            $nameMatch = !$NameFilter -or $_.Name -like $NameFilter
+            $versionMatch = !$VersionFilter -or $_.Version -like $VersionFilter 
             $nameMatch -and $versionMatch
 
         }
+        $allRefs = @($refs)
+        $assemblyChecked = @($AssemblyPath)
+        if ($Recurse) {
+            while ($refs) {
+                $refs = @($refs | ForEach-Object {
+                    $refPath="$([System.IO.Path]::GetDirectoryName($AssemblyPath))\$($_.Name).dll"
+                    if ($refPath -notin $assemblyChecked) {
+                        $a = @{
+                            AssemblyPath  = $refPath
+                            NameFilter    = $NameFilter 
+                            VersionFilter = $VersionFilter
+                            Recurse       = $Recurse
+                        }
+                        Get-AssemblyReference  @a
+                        $assemblyChecked += $refPath
+                    }
+                })
+                $allRefs += @($refs)
+            }
+        }
+        
+        $allRefs | Sort-Object Name -Unique
         $assembly.dispose()
     }
     
