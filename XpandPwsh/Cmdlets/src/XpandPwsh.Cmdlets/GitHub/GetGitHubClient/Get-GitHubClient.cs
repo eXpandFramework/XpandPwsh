29f230c83ba671b32dc243b1fd32fc5934d80d75
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2020-02-24 15:22:22 +0200

    Get-GitHubClient, Get-GitHubIssueEvents

---
+++ XpandPwsh/Cmdlets/src/XpandPwsh.Cmdlets/GitHub/GetGitHubClient/Get-GitHubClient.cs
@@ -0,0 +1,18 @@
+﻿using System.Management.Automation;
+using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace XpandPwsh.Cmdlets.GitHub.GetGitHubClient{
+    [CmdletBinding]
+    [Cmdlet(VerbsCommon.Get, "GitHubClient")]
+    [PublicAPI]
+    public class GetGitHubClient : GitHubCmdlet{
+        protected override Task ProcessRecordAsync(){
+            return Observable.Return(GitHubClient)
+                .WriteObject(this)
+                .ToTask();
+        }
+    }
+}
---
+++ XpandPwsh/Cmdlets/src/XpandPwsh.Cmdlets/GitHub/GetGitHubIssueEvents/Get-GitHubIssueEvents.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Management.Automation;
+using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Octokit;
+using XpandPwsh.CmdLets;
+
+namespace XpandPwsh.Cmdlets.GitHub.GetGitHubIssueEvents{
+    [CmdletBinding]
+    [Cmdlet(VerbsCommon.Get, "GitHubIssueEvents")]
+    [PublicAPI]
+    public class GetGitHubIssueEvents : GitHubCmdlet{
+        [Parameter(Mandatory = true)]
+        public string Repository{ get; set; }
+        [Parameter(Mandatory = true)]
+        public int IssueNumber{ get; set; }
+        
+        protected override Task ProcessRecordAsync(){
+            return GitHubClient.Repository.GetForOrg(Organization, Repository)
+                .SelectMany(repository => GitHubClient.Issue.Events.GetAllForIssue(repository.Id, IssueNumber))
+                .WriteObject(this)
+                .ToTask();
+        }
+    }
+}
