19dbc1ca67397fff6370788b8c1e1751e284b485
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2020-02-28 20:56:11 +0200

    Remove-GitHubRelease

---
+++ XpandPwsh/Cmdlets/src/XpandPwsh.Cmdlets/GitHub/GitHubRelease/Remove-GitHubRelease.cs
@@ -0,0 +1,27 @@
+﻿using System.Management.Automation;
+using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Octokit;
+using XpandPwsh.CmdLets;
+
+namespace XpandPwsh.Cmdlets.GitHub.GitHubRelease{
+    [CmdletBinding()]
+    [Cmdlet(VerbsCommon.Remove, "GitHubRelease")]
+    [OutputType(typeof(Release))]
+    [CmdLetTag(CmdLetTag.GitHub,CmdLetTag.Reactive,CmdLetTag.RX)][PublicAPI]
+    public class RemoveGitHubRelease : GitHubCmdlet{
+        [Parameter(Mandatory = true)]
+        public string Repository{ get; set; }
+        [Parameter]
+        public int ReleaseId{ get; set; }
+        protected override  Task ProcessRecordAsync(){
+            return GitHubClient.Repository.GetForOrg(Organization, Repository)
+                .SelectMany(repository => GitHubClient.Repository.Release.Delete(repository.Id,ReleaseId).ToObservable())
+                .HandleErrors(this,Repository)
+                .WriteObject(this)
+                .ToTask();            
+        }
+    }
+}
