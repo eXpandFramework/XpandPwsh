aa77a7a943dc907154ce02e35d45c768e86ce702
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2020-03-11 02:41:42 +0200

    ConvertTo-PackageObject: FileInfo conversion

--- XpandPwsh/Public/Nuget/ConvertTo-PackageObject.ps1
+++ XpandPwsh/Public/Nuget/ConvertTo-PackageObject.ps1
@@ -2,8 +2,8 @@ function ConvertTo-PackageObject {
     [CmdletBinding()]
     [CmdLetTag("#nuget")]
     param (
-        [parameter(ValueFromPipeline,Mandatory)]
-        [string]$item
+        [parameter(ValueFromPipeline)]
+        $item
     )
     
     begin {
@@ -11,18 +11,36 @@ function ConvertTo-PackageObject {
     }
     
     process {
-        if ($item -eq "No packages found."){
+        if ($item -eq "No packages found." -or !$item) {
             return
         }
-        $strings = $item.Split(" ")
-        if ($strings.Length -eq 2){
-            $v=new-object System.Version ($strings[1])
+        $strings = "$item".Split(" ")
+        if ($strings.Length -eq 2) {
+            $v = New-Object System.Version ($strings[1])
+            [PSCustomObject]@{
+                Id      = $strings[0]
+                Version = $v
+            }
         }
-        $psobj=[PSCustomObject]@{
-            Id    = $strings[0]
-            Version = $v
+        else {
+            $name=$item
+            if ($item -is [System.IO.FileInfo]){
+                $name=$item.basename
+            }
+
+            $regex = [regex] '(?imn)\.(?<version>[\d]+\.[\d]+\.[\d]+(\.[\d]+)?)$'
+            $v = $regex.Match($name).Groups["version"].Value
+            $id=$name.replace(".$v","")
+            $o = @{
+                Id = $id
+                Version=$v
+            }
+            if ($item -is [System.IO.FileInfo]){
+                $o.Add("File",$item)
+            }
+            [pscustomObject]$o
         }
-        $psobj
+        
     }
     
     end {
