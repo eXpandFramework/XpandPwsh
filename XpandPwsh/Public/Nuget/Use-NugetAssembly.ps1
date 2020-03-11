945c81a8468a4d367f3381cf61ee1d45b406e1b5
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2020-03-11 02:44:08 +0200

    Use-NugetAssembly: version arg

--- XpandPwsh/Public/Nuget/Use-NugetAssembly.ps1
+++ XpandPwsh/Public/Nuget/Use-NugetAssembly.ps1
@@ -6,15 +6,16 @@ function Use-NugetAssembly {
         [string]$packageName,
         [string]$framework = "*",
         [string]$OutputFolder = "$env:TEMP\$packageName",
-        [string]$Source = (Get-PackageFeed -Nuget)
+        [string]$Source = (Get-PackageFeed -Nuget),
+        [version]$Version
     )
     
     begin {
     }
     
     process {
-        $package=Get-NugetPackage -name $packageName -OutputFolder $OutputFolder -Source $Source
-        $package | where-object { $_.DotnetFramework -like $framework } | ForEach-Object {
+        $package=Get-NugetPackage -name $packageName -OutputFolder $OutputFolder -Source $Source -Versions $Version
+        $package | where-object { $_.DotnetFramework -match $framework } | ForEach-Object {
             $v = [version]$_.Version
             $version = "$($v.Major).$($v.Minor).$($v.Build)"
             $fullName = "$OutputFolder\$packagename\$version\$($_.File)"
