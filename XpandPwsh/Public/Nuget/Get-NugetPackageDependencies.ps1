3e4a72b1cee9da3472d0105bffe1fab85bc6ed62
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2020-03-11 02:42:20 +0200

    Get-NugetPackageDependencies :code format

--- XpandPwsh/Public/Nuget/Get-NugetPackageDependencies.ps1
+++ XpandPwsh/Public/Nuget/Get-NugetPackageDependencies.ps1
@@ -6,60 +6,62 @@ function Get-NugetPackageDependencies {
         [string]$Id,
         [string]$Version,
         [switch]$AllVersions,
-        [ArgumentCompleter({
-            [OutputType([System.Management.Automation.CompletionResult])]  # zero to many
-            param(
-                [string] $CommandName,
-                [string] $ParameterName,
-                [string] $WordToComplete,
-                [System.Management.Automation.Language.CommandAst] $CommandAst,
-                [System.Collections.IDictionary] $FakeBoundParameters
-            )
+        [ArgumentCompleter( {
+                [OutputType([System.Management.Automation.CompletionResult])]  # zero to many
+                param(
+                    [string] $CommandName,
+                    [string] $ParameterName,
+                    [string] $WordToComplete,
+                    [System.Management.Automation.Language.CommandAst] $CommandAst,
+                    [System.Collections.IDictionary] $FakeBoundParameters
+                )
             
-            (Get-packageSource).Name|where-object{$_ -like "$wordToComplete*"}
-        })]
-        [string]$Source=(Get-PackageFeed -Nuget),
-        [string]$Filter="*",
-        [switch]$Recurse
+                (Get-PackageSource).Name | Where-Object { $_ -like "$wordToComplete*" }
+            })]
+        [string]$Source = (Get-PackageFeed -Nuget),
+        [string]$FilterRegex,
+        [switch]$Recurse,
+        [switch]$IncludeDelisted
     )
     
     begin {
-        if ($source -in (Get-packageSource).Name ){
-            $Source=Get-PackageSourceLocations -Name $Source
+        if ($source -in (Get-PackageSource).Name ) {
+            $Source = Get-PackageSourceLocations -Name $Source
         }
     }
     
     process {
-        $packageChecked=@($id)
-        $a=@{
-            Name=$Id
-            Source=$Source
-            AllVersions=$AllVersions
+        $packageChecked = @($id)
+        $a = @{
+            Name            = $Id
+            Source          = $Source
+            AllVersions     = $AllVersions
+            IncludeDelisted = $IncludeDelisted
         }
-        if ($Version){
-            $a.Add("Version",$Version)
+        if ($Version) {
+            $a.Add("Version", $Version)
         }
-        elseif ($AllVersion){
-            $a.Add("AllVersions",$AllVersions)
+        elseif ($AllVersion) {
+            $a.Add("AllVersions", $AllVersions)
         }
         
-        $deps=(Get-NugetPackageSearchMetadata @a).DependencySets.Packages|Get-Unique|Where-Object{$_.id -like $Filter}
-        $allDeps=$deps
-        if ($Recurse){
+        $deps = (Get-NugetPackageSearchMetadata @a).DependencySets.Packages | Get-Unique | Where-Object { $_.id -match $FilterRegex }
+        $allDeps = $deps
+        if ($Recurse) {
             while ($deps) {
-                $deps=$deps|ForEach-Object{
-                    if ($_.id -notin $packageChecked){
-                        $a.Name=$_.Id
-                        (Get-NugetPackageSearchMetadata @a).DependencySets.Packages|Get-Unique|Where-Object{$_.id -like $Filter}
-                        $packageChecked+=$a.Name
+                $deps = $deps | ForEach-Object {
+                    if ($_.id -notin $packageChecked) {
+                        $a.Name = $_.Id
+                        (Get-NugetPackageSearchMetadata @a).DependencySets.Packages | Get-Unique | Where-Object { $_.id -match $FilterRegex }
+                        $packageChecked += $a.Name
                     }
                     
                 }
-                $allDeps+=$deps
+                $allDeps += $deps
             }
         }
         
-        $allDeps|Sort-Object Id -Unique
+        $allDeps | Sort-Object Id -Unique
     }
     
     end {
