79a679f37637939dc63d656ce50a5d37e8d7b1c8
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2020-03-16 20:04:55 +0200

    Update-OutputPath: handle web projects

--- XpandPwsh/Public/DotNet/Update-OutputPath.ps1
+++ XpandPwsh/Public/DotNet/Update-OutputPath.ps1
@@ -19,6 +19,17 @@ function Update-OutputPath {
         $outputPathSuffix=$CSProj.project.PropertyGroup.OutputPathSuffix
         $path+="\$outputPathSuffix"
         Update-ProjectProperty $CSProj OutputPath $path.Trim("\").Trim()
+        $projectDir=$((Get-Item $ProjectPath).DirectoryName)
+        if ((!(Test-ProjectSdk $CSProj)) -and (Test-Path "$projectDir\web.config") -and $OutputPath -ne "\bin" -and $OutputPath -ne "bin"){
+            $linkText='if exist "$(MSBuildProjectDirectory)\bin" rmdir "$(MSBuildProjectDirectory)\bin"`r`nmklink /J "$(MSBuildProjectDirectory)\bin" "$(MSBuildProjectDirectory)\$(OutputPath)"'
+            $postBuildEvent=$CSProj.project.PropertyGroup.PostBuildEvent|Where-Object{$_}
+            if (($postBuildEvent|Where-Object{
+                $_ -notlike "*$linkText*"
+            })){
+                Add-ProjectBuildEvent PostBuildEvent $CSProj -InnerText $linkText -Append
+            }
+        }
+        
     }
     
     end {
