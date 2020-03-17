0cbb07465f3c052ef8273e4eb0defb5e92ab20fc
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2020-03-17 10:07:53 +0200

    Use-NugetAssembly: improve framework filtering

--- XpandPwsh/Public/Nuget/Use-NugetAssembly.ps1
+++ XpandPwsh/Public/Nuget/Use-NugetAssembly.ps1
@@ -5,7 +5,7 @@ function Use-NugetAssembly {
         [parameter(ValueFromPipeline)]
         [string]$packageName,
         [ValidateSet("NETFramework","NETStandard","NETCoreApp")]
-        [string[]]$framework="NETFramework",
+        [string[]]$Framework,
         [string]$OutputFolder = "$env:TEMP\$packageName",
         [string]$Source = (Get-PackageFeed -Nuget),
         [version]$Version
@@ -17,7 +17,15 @@ function Use-NugetAssembly {
     
     process {
         $package=Get-NugetPackage -name $packageName -OutputFolder $OutputFolder -Source $Source -Versions $Version
-        $package | where-object { $_.DotnetFramework -like "*$framework*" } | ForEach-Object {
+        $frameworks=$package.DotnetFramework
+        $frameworkFilter="*$Framework*"
+        if ($frameworks.count -gt 1 -and !$Framework){
+            throw "Multiple frameworks found ($frameworks) for $packageName. Please use the Framework parameter."
+        }
+        elseif ($frameworks.count -eq 1 -and !$Framework){
+            $frameworkFilter="*"
+        }
+        $package | where-object { $_.DotnetFramework -like $frameworkFilter } | ForEach-Object {
             $v = [version]$_.Version
             $version = "$($v.Major).$($v.Minor).$($v.Build)"
             $fullName = "$OutputFolder\$packagename\$version\$($_.File)"
