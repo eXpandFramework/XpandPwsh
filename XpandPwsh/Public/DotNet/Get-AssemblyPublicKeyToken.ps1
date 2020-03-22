bce8d99e1ec74f60f4eb838e4e5465f5196ec1d4
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2020-03-22 16:03:52 +0200

    Get-AssemblyPublicKeyToken: directly from bytes

--- XpandPwsh/Public/DotNet/Get-AssemblyPublicKeyToken.ps1
+++ XpandPwsh/Public/DotNet/Get-AssemblyPublicKeyToken.ps1
@@ -2,17 +2,25 @@ function Get-AssemblyPublicKeyToken {
     [CmdletBinding()]
     [CmdLetTag("#dotnet")]
     param (
-        [parameter(ValueFromPipeline,Mandatory)]
-        [System.IO.FileInfo]$Assembly
+        [parameter(ValueFromPipeline,Mandatory,ParameterSetName="File")]
+        [System.IO.FileInfo]$Assembly,
+        [parameter(ValueFromPipeline,Mandatory,ParameterSetName="Raw")]
+        [byte[]]$Bytes
     )
     
     begin {
+        $PSCmdlet|Write-PSCmdLetBegin
+        Use-MonoCecil|Out-Null
     }
     
     process {
+        if ($PSCmdlet.ParameterSetName -eq "File"){
+            Use-Object($asm=Read-AssemblyDefinition $Assembly.FullName){
+                Get-AssemblyPublicKeyToken -bytes $asm.Name.publicKeyToken
+            }
+        }
         
-        $asm=Mount-Assembly $Assembly.FullName
-        (($asm.GetName().GetPublicKeyToken()|ForEach-Object{
+        (($Bytes|ForEach-Object{
             $_.ToString("x2")
         }) -join "").Trim("")
     }
