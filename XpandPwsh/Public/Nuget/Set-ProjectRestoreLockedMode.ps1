c8049fa4266a70a2f863834cfe829ec4e30c0c70
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2020-03-28 11:30:32 +0200

    Set-ProjectRestoreLockedMode: add ProjectFile param

--- XpandPwsh/Public/Nuget/Set-ProjectRestoreLockedMode.ps1
+++ XpandPwsh/Public/Nuget/Set-ProjectRestoreLockedMode.ps1
@@ -1,9 +1,12 @@
 function Set-ProjectRestoreLockedMode {
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName="Project")]
     [CmdLetTag("#nuget")]
     param (
-        [parameter(Mandatory,ValueFromPipelineByPropertyName)]
+        [parameter(Mandatory,ValueFromPipeline,ParameterSetName="Project",Position=0)]
         [xml]$Project,
+        [parameter(Mandatory,ValueFromPipeline,ParameterSetName="File",Position=0)]
+        [System.IO.FileInfo]$ProjectFile,
+        [parameter(Position=1)]
         [bool]$Value=$true
     )
     
@@ -12,10 +15,27 @@ function Set-ProjectRestoreLockedMode {
     }
     
     process {
+        if ($PSCmdlet.ParameterSetName -eq "File"){
+            $Project=Get-XmlContent $ProjectFile.FullName
+        }
         Update-ProjectProperty  $Project RestorePackagesWithLockFile $Value
         Update-ProjectProperty  $Project RestoreLockedMode $Value
         Update-ProjectProperty  $Project NoWarn NU1603
         Update-ProjectProperty  $Project DisableImplicitNuGetFallbackFolder $Value
+        
+        if ($PSCmdlet.ParameterSetName -eq "File"){
+            if (!$Value){
+                $lockFile="$($ProjectFile.DirectoryName)\packages.lock.json"
+                if (Test-Path $lockFile){
+                    Remove-Item  $lockFile
+                }
+            }
+            $include=$Project.Project.ItemGroup.Content|Where-Object{$_.Include -eq "packages.lock.json"}
+            if ($include){
+                $include.ParentNode.RemoveChild($include)
+            }
+            $Project|Save-Xml $ProjectFile.FullName|Out-Null
+        }
     }
     
     end {
