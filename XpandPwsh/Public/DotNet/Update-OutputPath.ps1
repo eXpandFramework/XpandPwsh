dece1fe1a060fd7ff26485d9b9c9f7cf7f52ba5f
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2020-04-04 12:52:55 +0300

    Update-OutputPath: post build event wrong

--- XpandPwsh/Public/DotNet/Update-OutputPath.ps1
+++ XpandPwsh/Public/DotNet/Update-OutputPath.ps1
@@ -20,7 +20,10 @@ function Update-OutputPath {
         Update-ProjectProperty $CSProj OutputPath $path.Trim("\").Trim()
         $projectDir=$((Get-Item $ProjectPath).DirectoryName)
         if ((!(Test-ProjectSdk $CSProj)) -and (Test-Path "$projectDir\web.config") -and $OutputPath -ne "\bin" -and $OutputPath -ne "bin"){
-            $linkText='if exist "$(MSBuildProjectDirectory)\bin" rmdir "$(MSBuildProjectDirectory)\bin"`r`nmklink /J "$(MSBuildProjectDirectory)\bin" "$(MSBuildProjectDirectory)\$(OutputPath)"'
+            $linkText=@"
+if exist `"`$(MSBuildProjectDirectory)\bin`" rmdir `"`$(MSBuildProjectDirectory)\bin`"
+nmklink /J `"`$(MSBuildProjectDirectory)\bin" "`$(MSBuildProjectDirectory)\`$(OutputPath)`"
+"@
             $postBuildEvent=$CSProj.project.PropertyGroup.PostBuildEvent|Where-Object{$_}
             if (($postBuildEvent|Where-Object{
                 $_ -notlike "*nmklink*"
