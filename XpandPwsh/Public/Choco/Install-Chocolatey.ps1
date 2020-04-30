d75c855ff42ce4eba6f3e42d9346c9149d8c480d
apobekiaris <apostolis.bekiaris@gmail.com>
2020-04-30 13:45:17 +0300

    Install-Chocolatey: remove messages

--- XpandPwsh/Public/Choco/Install-Chocolatey.ps1
+++ XpandPwsh/Public/Choco/Install-Chocolatey.ps1
@@ -11,24 +11,16 @@ function Install-Chocolatey {
     }
     
     process {
-        if (Test-ChocoInstalled){
-            return
-        }
-        "verifying chocolatey is installed"
-        if (!(Test-Path "$($env:ProgramData)\chocolatey\choco.exe")) {
-            "installing chocolatey..."
+        if (!(Test-ChocoInstalled)) {
             Set-ExecutionPolicy Bypass -Scope Process -Force;
             if (!(Test-path "$env:ChocolateyPath\lib")){
                 New-Item "$env:ChocolateyPath\lib" -ItemType Directory
             }
             Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-            if ($AllowGlobalConfirmation){
-                choco feature enable -n=allowGlobalConfirmation
-            }
         }
-        else {
-            "chocolatey is already installed"
-        }          
+        if ($AllowGlobalConfirmation){
+            choco feature enable -n=allowGlobalConfirmation
+        }
     }
     
     end {
