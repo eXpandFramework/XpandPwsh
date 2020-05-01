92ac83954d7f062c99b2c75d55d07c1b26e446bc
apobekiaris <apostolis.bekiaris@gmail.com>
2020-05-01 09:26:35 +0300

    Find-Tweet, Include,Excude,count args

--- XpandPwsh/Cmdlets/src/XpandPwsh.Cmdlets/Twitter/Find-Tweet.cs
+++ XpandPwsh/Cmdlets/src/XpandPwsh.Cmdlets/Twitter/Find-Tweet.cs
@@ -20,11 +20,21 @@ namespace XpandPwsh.Cmdlets.Twitter{
         public string ScreenName{ get; set; }
 
         [Parameter]
+        [ValidateRange(0, 200)]
+        public int Count{ get; set; } = 200;
+        [Parameter]
         public string MatchPattern{ get; set; } = ".*";
+        [Parameter]
+        public bool ExcludeReplies{ get; set; } = true;
+        [Parameter]
+        public bool IncludeRetweets{ get; set; } 
 
         protected override Task ProcessRecordAsync(){
             
             return TwitterContext.Status.Where(status => status.Type==StatusType.User&&status.ScreenName==ScreenName)
+                .Where(status => Count==0||Count==status.Count)
+                .Where(status => status.ExcludeReplies==ExcludeReplies)
+                .Where(status => status.IncludeRetweets==IncludeRetweets)
                 .ToListAsync().ToObservable().SelectMany(list => list)
                 .Where(status => new Regex(MatchPattern).IsMatch(status.Text))
                 .HandleErrors(this)
