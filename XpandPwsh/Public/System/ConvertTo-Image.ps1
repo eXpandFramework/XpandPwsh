6890197f5979a860afe74450786641411a0949a2
apobekiaris <apostolis.bekiaris@gmail.com>
2020-06-16 14:31:59 +0300

    ConvertTo-Image: resize canvas only when smaller

--- XpandPwsh/Public/System/ConvertTo-Image.ps1
+++ XpandPwsh/Public/System/ConvertTo-Image.ps1
@@ -7,7 +7,9 @@ function ConvertTo-Image {
         [parameter(Mandatory)]
         [string]$OutputFile,
         [int]$MaximumSizeBytes,
-        [int]$MaximumWidth
+        [int]$MaximumWidth,
+        [int]$Width,
+        [int]$MinimumCanvasHeight
     )
     
     begin {
@@ -33,12 +35,26 @@ function ConvertTo-Image {
                     Invoke-Script{& "$ImageMagick" .\$baseName.png -resize $MaximumWidth ".\$baseName.resized.png" }
                     $baseName="$baseName.resized"
                 }
+                $MaximumWidth=$MaximumWidth*0.9
             }
             Invoke-Script{& "$ImageMagick" .\$baseName.png -flatten -fuzz 1% -trim +repage ".\$baseName.trim.png" }
             $baseName=".\$baseName.trim"
             Invoke-Script{& "$ImageMagick" .\$baseName.png -bordercolor white -border 20 ".\$baseName.border.png" }
             $baseName=".\$baseName.border"
-            $MaximumWidth=$MaximumWidth*0.9
+            if ($Width){
+                $bmp=[System.Drawing.Bitmap]::new((Get-Item .\$baseName.png).FullName)
+                if ($bmp.Width -ne $Width){
+                    Invoke-Script{& "$ImageMagick" .\$baseName.png -resize $Width ".\$baseName.resizedWidth.png" }
+                    $baseName="$baseName.resizedWidth"
+                }
+            }
+            if ($MinimumCanvasHeight){
+                $bmp=[System.Drawing.Bitmap]::new((Get-Item .\$baseName.png).FullName)
+                if ($bmp.Height -lt $MinimumCanvasHeight){
+                    Invoke-Script{& "$ImageMagick" convert .\$baseName.png -background none -gravity center -extent "$($bmp.Width)x$CanvasHeight" ".\$baseName.extendCanvas.png" }
+                    $baseName="$baseName.extendCanvas"
+                }
+            }
         } while ($MaximumSizeBytes -and (([System.IO.File]::ReadAllBytes("$(Get-Location)\$baseName.png")).Length -gt $MaximumSizeBytes))
         Copy-Item ".\$baseName.png" $OutputFile -Force 
         Pop-Location
