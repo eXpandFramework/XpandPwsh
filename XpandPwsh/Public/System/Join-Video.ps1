c69dc11b2c903d19a093b1e0d760f1318f768fc1
apobekiaris <apostolis.bekiaris@gmail.com>
2020-07-31 19:13:45 +0300

    Join-Video: wait for the process to finish

--- XpandPwsh/Public/System/Join-Video.ps1
+++ XpandPwsh/Public/System/Join-Video.ps1
@@ -24,20 +24,18 @@ function Join-Video {
     }
     
     end {
-        Invoke-Script{
-            Push-Location (Get-Item $Video|Select-Object -First 1).DirectoryName
-            $format=[System.IO.Path]::GetExtension($outputFile).Substring(1)
-            Remove-Item $outputFile -ErrorAction SilentlyContinue
-            
-            $e+="-filter_complex concat=n=$($e.Length):v=1:a=0"
-            $e+="-f $format"
-            $e+="-vn"
-            $e+="-y"
-            $e+=$outputFile
-            Start-Process ffmpeg.exe $e -WorkingDirectory (Get-Location) -NoNewWindow
-            Get-Item $OutputFile
-            Pop-Location
-        }
+        Push-Location (Get-Item $Video|Select-Object -First 1).DirectoryName
+        $format=[System.IO.Path]::GetExtension($outputFile).Substring(1)
+        Remove-Item $outputFile -ErrorAction SilentlyContinue
+        
+        $e+="-filter_complex `"concat=n=$($e.Length):v=1:a=0`""
+        $e+="-f $format"
+        $e+="-vn"
+        $e+="-y"
+        $e+=$outputFile
+        Start-Process ffmpeg.exe $e -WorkingDirectory (Get-Location) -NoNewWindow -Wait
+        Get-Item $OutputFile
+        Pop-Location
         
     }
 }
