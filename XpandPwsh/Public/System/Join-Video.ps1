30270f8ab45a4d3b44cb12a5083adf1880b5a330
apobekiaris <apostolis.bekiaris@gmail.com>
2020-07-31 14:01:53 +0300

    Join-Video: works on Azure

--- XpandPwsh/Public/System/Join-Video.ps1
+++ XpandPwsh/Public/System/Join-Video.ps1
@@ -17,28 +17,17 @@ function Join-Video {
     }
     
     process {
-        $Video|ForEach-Object{
-            Push-Location $_.DirectoryName
-            $transCode="$($_.BaseName).ts"
-            Remove-Item $transCode -Force -ErrorAction SilentlyContinue
-            $files+=$transCode
-            $name=$_.Name
-            Invoke-Script{
-                ffmpeg -i $name -c copy -bsf:v h264_mp4toannexb -f mpegts $transCode
-            }
-            Pop-Location
-        }
+        $files+="-i"
+        $files+=$Video.FullName
     }
     
     end {
         Remove-Item $OutputFile -ErrorAction SilentlyContinue
         Push-Location (Get-Item $Video|Select-Object -First 1).DirectoryName
         Invoke-Script{
-            
-            ffmpeg -i "concat:$(($files|Join-String -Separator '|'))" -c copy -bsf:a aac_adtstoasc $OutputFile
-            
+            $format=[System.IO.Path]::GetExtension($outputFile)
+            ffmpeg @files -filter_complex "concat=n=$($files.Length):v=1:a=0" -f $format -vn -y $outputFile   
         }
-        $files|Remove-Item -ErrorAction SilentlyContinue
         Get-Item $OutputFile
         Pop-Location
     }
