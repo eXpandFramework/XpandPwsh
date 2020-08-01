b00d1ec0874fb1796faeb3aa1fd5c51af3b0e959
apobekiaris <apostolis.bekiaris@gmail.com>
2020-08-02 02:13:59 +0300

    1.202.40.20

--- XpandPwsh/Public/System/Optimize-Gif.ps1
+++ XpandPwsh/Public/System/Optimize-Gif.ps1
@@ -16,9 +16,9 @@ function Optimize-Gif {
     process {
         $palette="$env:TEMP\palette.png"
         $filters="fps=$FrameRate,scale=$Scale`:-1:flags=lanczos"
-        Invoke-Script{ffmpeg -v warning -i $Gif.FullName -vf "$filters,palettegen=stats_mode=diff" -y $palette}
+        Invoke-Script{ffmpeg -hide_banner -loglevel panic -i $Gif.FullName -vf "$filters,palettegen=stats_mode=diff" -y $palette}
         $ffmpegOutput="$($Gif.DirectoryName)\$($Gif.BaseName)_ffmpeg$($Gif.Extension)"
-        Invoke-Script{ffmpeg -v warning -i $Gif.FullName -i $palette -lavfi "$filters,paletteuse=dither=bayer:bayer_scale=5:diff_mode=rectangle" -y $ffmpegOutput}
+        Invoke-Script{ffmpeg -hide_banner -loglevel panic -i $Gif.FullName -i $palette -lavfi "$filters,paletteuse=dither=bayer:bayer_scale=5:diff_mode=rectangle" -y $ffmpegOutput}
         $gifsicleOutput="$($Gif.DirectoryName)\$($Gif.BaseName)_optimized$($Gif.Extension)"
         Invoke-Script{gifsicle -O3 $ffmpegOutput -o $gifsicleOutput}
         Get-Item $gifsicleOutput
--- XpandPwsh/XpandPwsh.psd1
+++ XpandPwsh/XpandPwsh.psd1
@@ -12,7 +12,7 @@
 RootModule = 'XpandPwsh.psm1'
 
 # Version number of this module.
-ModuleVersion = '1.202.40.19'
+ModuleVersion = '1.202.40.20'
 
 # Supported PSEditions
 # CompatiblePSEditions = @()
