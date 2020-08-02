1e6bf5fdc007c8213393fcc16cba80691dee98ab
apobekiaris <apostolis.bekiaris@gmail.com>
2020-08-02 16:28:49 +0300

    Optimize-Gif: gifsicle is superior

--- XpandPwsh/Public/System/Optimize-Gif.ps1
+++ XpandPwsh/Public/System/Optimize-Gif.ps1
@@ -17,7 +17,7 @@ function Optimize-Gif {
         # $palette="$env:TEMP\palette.png"
         # $filters="fps=$FrameRate,scale=$Scale`:-1:flags=lanczos"
         # Invoke-Script{ffmpeg -hide_banner -loglevel panic -i $Gif.FullName -vf "$filters,palettegen=stats_mode=diff" -y $palette}
-        # $ffmpegOutput="$($Gif.DirectoryName)\$($Gif.BaseName)_ffmpeg$($Gif.Extension)"
+        $ffmpegOutput="$($Gif.DirectoryName)\$($Gif.BaseName)_ffmpeg$($Gif.Extension)"
         # Invoke-Script{ffmpeg -hide_banner -loglevel panic -i $Gif.FullName -i $palette -lavfi "$filters,paletteuse=dither=bayer:bayer_scale=5:diff_mode=rectangle" -y $ffmpegOutput}
         $gifsicleOutput="$($Gif.DirectoryName)\$($Gif.BaseName)_optimized$($Gif.Extension)"
         Invoke-Script{gifsicle -O3 $ffmpegOutput -o $gifsicleOutput}
