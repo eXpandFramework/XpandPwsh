b5d6b2f765bd18d6d5939102a847eaec025a1789
apobekiaris <apostolis.bekiaris@gmail.com>
2020-10-10 22:55:27 +0300

    Add-XmlElement: do not add duplicates

--- XpandPwsh/Public/DotNet/Add-XmlElement.ps1
+++ XpandPwsh/Public/DotNet/Add-XmlElement.ps1
@@ -1,37 +1,51 @@
 function Add-XmlElement {
-    [CmdletBinding(DefaultParameterSetName="Parent")]
+    [CmdletBinding(DefaultParameterSetName = "Parent")]
     [CmdLetTag("#dotnet")]
     param (
-        [parameter(Mandatory,Position=0)]
+        [parameter(Mandatory, Position = 0)]
         [System.Xml.XmlDocument]$Owner,
-        [parameter(Mandatory,Position=1)]
+        [parameter(Mandatory, Position = 1)]
         [string]$ElementName,
-        [parameter(Mandatory,ParameterSetName="Parent",Position=2)]
+        [parameter(Mandatory, ParameterSetName = "Parent", Position = 2)]
         [string]$Parent,
-        [parameter(Position=3)]
+        [parameter(Position = 3)]
         [System.Collections.Specialized.OrderedDictionary]$Attributes,
-        [parameter(Position=4)]
+        [parameter(Position = 4)]
         [string]$InnerText,
-        [parameter(Mandatory,ParameterSetName="ParentNode",Position=5)]
+        [parameter(Mandatory, ParameterSetName = "ParentNode", Position = 5)]
         [System.Xml.XmlElement]$ParentNode
     )
     
     begin {
-        $PSCmdlet|Write-PSCmdLetBegin
+        $PSCmdlet | Write-PSCmdLetBegin
     }
     
     process {
         $ns = New-Object System.Xml.XmlNamespaceManager($Owner.NameTable)
-        $nsUri=$Owner.DocumentElement.NamespaceURI
+        $nsUri = $Owner.DocumentElement.NamespaceURI
         $ns.AddNamespace("ns", $nsUri)
-        $element = $Owner.CreateElement($ElementName, $nsUri)
-        if ($Attributes){
+        if ($Attributes) {
+            $attributesFilter="["
+            $attributesFilter=$Attributes.Keys | ForEach-Object {
+                "@$_='$($Attributes[$_])'"
+            } |Join-String -Separator " and "
+            $attributesFilter="[$attributesFilter]"
+        }
+        
+        $element = $Owner.SelectSingleNode("//ns:$ElementName$($attributesFilter)", $ns)
+        if (($ParentNode -and $element.ParentNode -ne $ParentNode) -or ($Parent -and $element.ParentNode.LocalName -ne $Parent)){
+            $element = $Owner.CreateElement($ElementName, $nsUri)
+        }
+        if ($Attributes) {
             $Attributes.Keys | ForEach-Object {
                 $element.SetAttribute($_, $Attributes[$_])
             }
         }
-        $element.InnerText=$InnerText;
-        if (!$ParentNode){
+        if ($InnerText){
+            $element.InnerText = $InnerText;
+        }
+        
+        if (!$ParentNode) {
             $parentNode = $Owner.SelectSingleNode("//ns:$Parent", $ns)
         }
         $parentNode.AppendChild($Owner.CreateTextNode([System.Environment]::NewLine)) | Out-Null
