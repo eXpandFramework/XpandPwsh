4ca0ca8e004cdb250e69a475af03672b96d58a40
apobekiaris <apostolis.bekiaris@gmail.com>
2020-12-06 21:51:25 +0200

    Update-AssemblyInfoVersion: classed version support

--- XpandPwsh/Public/DotNet/Update-AssemblyInfoVersion.ps1
+++ XpandPwsh/Public/DotNet/Update-AssemblyInfoVersion.ps1
@@ -20,6 +20,12 @@ function Update-AssemblyInfoVersion {
             $result = $c -creplace 'Version\("([^"]*)', "Version(""$version"
             Set-Content $_.FullName $result
         }        
+        Get-ChildItem -path $path -filter "*AssemblyInfoVersion.cs" -Recurse|ForEach-Object {
+            $c = Get-Content $_.FullName
+            $regex = [regex] '(?s)Version = "([^"]*)'
+            $result = $regex.Replace($c, "Version = `"$version")
+            Set-Content $_.FullName $result
+        }        
     }
     
     end {
