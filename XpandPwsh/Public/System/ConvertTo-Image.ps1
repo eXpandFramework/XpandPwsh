f455815feb3ed01e2cbb6cd189c796b9e1e68101
apobekiaris <apostolis.bekiaris@gmail.com>
2021-02-20 14:06:14 +0200

    ConvertTo-Image: Improve params collab

--- XpandPwsh/Public/System/ConvertTo-Image.ps1
+++ XpandPwsh/Public/System/ConvertTo-Image.ps1
@@ -27,8 +27,21 @@ function ConvertTo-Image {
         $mdFile=".\$baseName.md"
         Set-Content $mdFile $Text
         Invoke-Script{pretty-md-pdf -i $mdFile -t png}
+        $baseName=[System.IO.Path]::GetFileNameWithoutExtension($OutputFile)
+        Invoke-Script{& "$ImageMagick" .\$baseName.png -flatten -fuzz 1% -trim +repage ".\$baseName.trim.png" }
+        $baseName="$baseName.trim"
+        Invoke-Script{& "$ImageMagick" .\$baseName.png -bordercolor white -border 20 ".\$baseName.border.png" }
+        $baseName="$baseName.border"
+        if ($Width){
+            $bmp=[System.Drawing.Bitmap]::new((Get-Item .\$baseName.png).FullName)
+            if ($bmp.Width -ne $Width){
+                Invoke-Script{& "$ImageMagick" .\$baseName.png -resize $Width ".\$baseName.resizedWidth.png" }
+                $baseName="$baseName.resizedWidth"
+            }
+            $bmp.Dispose()
+        }
+        $resize=100
         do {
-            $baseName=[System.IO.Path]::GetFileNameWithoutExtension($OutputFile)
             if ($MaximumWidth){
                 $bmp=[System.Drawing.Bitmap]::new((Get-Item .\$baseName.png).FullName)
                 if ($bmp.Width -gt $MaximumWidth){
@@ -36,26 +49,25 @@ function ConvertTo-Image {
                     $baseName="$baseName.resized"
                 }
                 $MaximumWidth=$MaximumWidth*0.9
+                $bmp.Dispose()
             }
-            Invoke-Script{& "$ImageMagick" .\$baseName.png -flatten -fuzz 1% -trim +repage ".\$baseName.trim.png" }
-            $baseName=".\$baseName.trim"
-            Invoke-Script{& "$ImageMagick" .\$baseName.png -bordercolor white -border 20 ".\$baseName.border.png" }
-            $baseName=".\$baseName.border"
-            if ($Width){
-                $bmp=[System.Drawing.Bitmap]::new((Get-Item .\$baseName.png).FullName)
-                if ($bmp.Width -ne $Width){
-                    Invoke-Script{& "$ImageMagick" .\$baseName.png -resize $Width ".\$baseName.resizedWidth.png" }
-                    $baseName="$baseName.resizedWidth"
-                }
-            }
+            
             if ($MinimumCanvasHeight){
                 $bmp=[System.Drawing.Bitmap]::new((Get-Item .\$baseName.png).FullName)
                 if ($bmp.Height -lt $MinimumCanvasHeight){
                     Invoke-Script{& "$ImageMagick" convert .\$baseName.png -background none -gravity center -extent "$($bmp.Width)x$MinimumCanvasHeight" ".\$baseName.extendCanvas.png" }
                     $baseName="$baseName.extendCanvas"
                 }
+                $bmp.Dispose()
             }
-        } while ($MaximumSizeBytes -and (([System.IO.File]::ReadAllBytes("$(Get-Location)\$baseName.png")).Length -gt $MaximumSizeBytes))
+            $size=([System.IO.File]::ReadAllBytes("$(Get-Location)\$baseName.png")).Length
+            if ($MaximumSizeBytes -and ($size -gt $MaximumSizeBytes)){
+                $resize-=5
+                Invoke-Script{& "$ImageMagick" .\$baseName.png -resize "$resize%" ".\$baseName.maximumsize.png" }
+                $baseName="$baseName.maximumsize"
+                $size=([System.IO.File]::ReadAllBytes("$(Get-Location)\$baseName.png")).Length
+            }
+        } while ($MaximumSizeBytes -and ($size -gt $MaximumSizeBytes))
         Copy-Item ".\$baseName.png" $OutputFile -Force 
         Pop-Location
         Pop-Location
