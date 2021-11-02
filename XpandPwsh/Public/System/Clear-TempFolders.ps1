ba7b34c24f87863779836830e06d31bd77a215be
apobekiaris <apostolis.bekiaris@gmail.com>
2021-11-02 19:38:29 +0200

    Clear-TempFolders: env:temp suppor

--- XpandPwsh/Public/System/Clear-TempFolders.ps1
+++ XpandPwsh/Public/System/Clear-TempFolders.ps1
@@ -1,7 +1,9 @@
 function Clear-TempFolders {
     [CmdLetTag()]
     param(
-        $tempfolders = @( "C:\Windows\Temp\*", "C:\Windows\Prefetch\*", "C:\Documents and Settings\*\Local Settings\temp\*", "C:\Users\*\Appdata\Local\Temp\*" )
+        $tempfolders = @( "C:\Windows\Temp\*", "C:\Windows\Prefetch\*", "C:\Documents and Settings\*\Local Settings\temp\*", "C:\Users\*\Appdata\Local\Temp\*",$env:TEMP )
     )
-    Remove-Item $tempfolders -force -recurse
+    $tempfolders|ForEach-Object{
+        Get-ChildItem $_|Remove-Item -Force -Recurse -ErrorAction Continue
+    }
 }
