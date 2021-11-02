66840a1e9bd85af176d1f32072a405c15fa4d088
apobekiaris <apostolis.bekiaris@gmail.com>
2021-11-02 19:38:52 +0200

    Resolve-PortUsage

---
+++ XpandPwsh/Public/System/Resolve-PortUsage.ps1
@@ -0,0 +1,39 @@
+function Resolve-PortUsage {
+    [CmdletBinding()]
+    [CmdLetTag()]
+    param (
+        [parameter(ValueFromPipeline,ParameterSetName="port",Mandatory)]
+        [int]$Port,
+        [ArgumentCompleter( {
+            param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
+            ((Get-Process).Name | Where-Object { $_ -like "*$wordToComplete*" }).Name
+        })]
+        [parameter(ValueFromPipeline,ParameterSetName="process",Mandatory)]
+        [string]$Processname,
+        [switch]$Kill
+    )
+    
+    begin {
+        $PSCmdlet|Write-PSCmdLetBegin
+    }
+    
+    process {
+        if ($Processname){
+            $id=(Get-Process -Name DXApplication2.Win).Id
+            netstat /a /n /o | find " $id"
+            return
+        }
+        $proc=netstat -aon|select-string $Port|ForEach-Object{("$_".substring("$_".LastIndexOf(' '))).Trim()}|ForEach-Object{
+            $id=$_
+            Get-Process|Where-Object{$_.id -eq $id}
+        }
+        $proc
+        if ($Kill){
+            $proc|Stop-Process
+        }
+    }
+    
+    end {
+        
+    }
+}
