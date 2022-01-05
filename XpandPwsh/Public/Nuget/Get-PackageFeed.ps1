fe8ec20d1fea05bc6acb761fa1c09db946eff115
apobekiaris <apostolis.bekiaris@gmail.com>
2022-01-05 17:32:22 +0200

    get-Get-PackageFeed: add Azure feed

--- XpandPwsh/Public/Nuget/Get-PackageFeed.ps1
+++ XpandPwsh/Public/Nuget/Get-PackageFeed.ps1
@@ -6,7 +6,8 @@ function Get-PackageFeed {
         [switch]$Xpand,
         [switch]$Nuget,
         [switch]$GitHubLab,
-        [ValidateSet("Xpand","Nuget","Lab","Release")]
+        [switch]$AzDevOps,
+        [ValidateSet("Xpand","Nuget","Lab","Release","AzDevOps")]
         [string]$FeedName
     )
     
@@ -24,12 +25,18 @@ function Get-PackageFeed {
         if ($GitHubLab){
             "https://nuget.pkg.github.com/eXpandFramework/index.json"
         }
+        if ($AzDevOps){
+            "https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet-tools/nuget/v3/index.json"
+        }
         if ($FeedName){
             if ($FeedName -eq "lab"){
                 $fname="Xpand"
             }
             if ($FeedName -eq "Release"){
                 $fname="Nuget"
+            }
+            if ($FeedName -eq "AzDevOps"){
+                $fname="AzDevOps"
             }
             $a = @{
                 $fname = $true
